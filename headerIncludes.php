6baa02a0b9a47410bac4ca7b69dbaac5ec97d346
msblake <msblake@ca2c5cd7-39d7-495c-9a21-ea95cd046ca5>
2011-04-06 19:56:29 +0000

    Moved JqueryUI from engineIncludes to the application includes directory.

--- headerIncludes.php
+++ headerIncludes.php
@@ -3,8 +3,9 @@ global $engine;
 ?>
 
 <script type="text/javascript" src="/engineIncludes/jquery.ui.js"></script>
-<script type="text/javascript" src="{local var="siteRoot"}includes/jquery-validate.js"></script>
 <script type="text/javascript" src="{local var="siteRoot"}includes/jquery-expose.js"></script>
+<script type="text/javascript" src="{local var="siteRoot"}includes/jquery-validate.js"></script>
+<script type="text/javascript" src="{local var="siteRoot"}includes/jquery-ui.js"></script>
 <script type="text/javascript" src="{local var="siteRoot"}includes/functions.js"></script>
 <script type="text/javascript" src="{local var="siteRoot"}includes/dynamic.js"></script>
 <script type="text/javascript" src="{local var="siteRoot"}includes/validate.js"></script>
