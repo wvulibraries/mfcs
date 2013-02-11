9a8daebc932d3e445f99e64aac9861342564ece4
Scott Blake <Scott.Blake@mail.wvu.edu>
2013-02-11 13:05:00 -0500

    Added forms and fields tables to dbTableList

--- dbTableList.php
+++ dbTableList.php
@@ -1,4 +1,6 @@
 <?php
 $engine->dbTables("containers","prod","containers");
 $engine->dbTables("objectTypes","prod","objectTypes");
+$engine->dbTables("fields","prod","fields");
+$engine->dbTables("forms","prod","forms");
 ?>
