3fa1d49a3a2638e18fcaa300d24e969206be4b1d
MichaelRBond <mbond@the-forgotten.org>
2013-02-12 10:56:22 -0500

    field en/decode functions

--- includes/functions.php
+++ includes/functions.php
@@ -7,4 +7,16 @@ function displayMessages() {
 	return '<section><header><h1>Results</h1></header>'.errorHandle::prettyPrint().'</section>';
 }
 
+function encodeFields($fields) {
+
+	return base64_encode(serialize($fields));
+
+}
+
+function decodeFields($fields) {
+
+	return unserialize(base64_decode($fields));
+
+}
+
 ?>
