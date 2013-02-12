d08b0fa206e315a9676d988741f4d696fd254c21
MichaelRBond <mbond@the-forgotten.org>
2013-02-12 11:24:57 -0500

    turn on debug to log

--- engineInclude.php
+++ engineInclude.php
@@ -1,4 +1,6 @@
 <?php
-require_once("/home/dev.systems.lib.wvu.edu/phpincludes/engine/engineAPI/3.1/engine.php");
+require_once("/home/dev1.systems.lib.wvu.edu/phpincludes/engine/engineAPI/3.1/engine.php");
 $engine = EngineAPI::singleton();
+
+errorHandle::errorReporting(errorHandle::E_ALL);
 ?>
