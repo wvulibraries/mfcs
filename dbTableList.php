6ff1f72cb727fce1e0e0013f03a42339d86fb1f7
MichaelRBond <mbond@the-forgotten.org>
2013-02-13 12:48:57 -0500

    Users table in db list

--- dbTableList.php
+++ dbTableList.php
@@ -4,4 +4,5 @@ $engine->dbTables("fields","prod","fields");
 $engine->dbTables("forms","prod","forms");
 $engine->dbTables("objectTypes","prod","objectTypes");
 $engine->dbTables("projects","prod","projects");
+$engine->dbTables("users","prod","users");
 ?>
