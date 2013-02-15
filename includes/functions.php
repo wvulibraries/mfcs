aa1543b089671e89cf948efdb650471d739cec50
MichaelRBond <mbond@the-forgotten.org>
2013-02-15 13:53:05 -0500

    check to see if the currently logged in user has permissions to a project.

--- includes/functions.php
+++ includes/functions.php
@@ -19,4 +19,31 @@ function decodeFields($fields) {
 
 }
 
+function checkProjectPermissions($id) {
+
+	$engine = EngineAPI::singleton();
+
+	$username = sessionGet("username");
+
+	$sql       = sprintf("SELECT COUNT(permissions.ID) FROM permissions LEFT JOIN users on users.ID=permissions.userID WHERE permissions.projectID='%s' AND users.username='%s'",
+		$engine->openDB->escape($id),
+		$engine->openDB->escape($username)
+		);
+	$sqlResult = $engine->openDB->query($sql);
+	
+	if (!$sqlResult['result']) {
+		errorHandle::newError(__METHOD__."() - ".$sqlResult['error'], errorHandle::DEBUG);
+		return(FALSE);
+	}
+	
+	$row       = mysql_fetch_array($sqlResult['result'],  MYSQL_ASSOC);
+
+	if ((int)$row['COUNT(permissions.ID)'] > 0) {
+		return(TRUE);
+	}
+
+	return(FALSE);
+
+}
+
 ?>
