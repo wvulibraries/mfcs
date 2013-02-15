5f3f2072966deabb922531128fe55d4ffe79336b
Scott Blake <Scott.Blake@mail.wvu.edu>
2013-02-15 14:12:39 -0500

    Upgrade JQueryUI to 1.10.0

--- headerIncludes.php
+++ headerIncludes.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" type="text/css" href="{local var="siteRoot"}includes/css/mfcs.css">
 
-<script type="text/javascript" src="{local var="siteRoot"}includes/js/jquery-ui-1.9.2.custom.min.js"></script>
+<script type="text/javascript" src="{local var="siteRoot"}includes/js/jquery-ui-1.10.0.custom.min.js"></script>
 <script type="text/javascript" src="{local var="siteRoot"}includes/js/jquery.ui.touch-punch.min.js"></script>
