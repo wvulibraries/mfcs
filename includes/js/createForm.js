47991ed70aa2d8ef1ac5c3d328de50334786e5eb
Scott Blake <Scott.Blake@mail.wvu.edu>
2013-03-11 10:27:30 -0400

    Add second No Duplicates checkbox

--- includes/js/createForm.js
+++ includes/js/createForm.js
@@ -219,6 +219,10 @@ function showFieldSettings(fullID) {
 					checked:  true,
 					disabled: true,
 				}).change();
+				$("#fieldSettings_options_duplicatesForm").prop({
+					checked:  false,
+					disabled: true,
+				}).change();
 				$("#fieldSettings_options_readonly").prop("disabled",true);
 				$("#fieldSettings_options_disabled").removeAttr("checked").change().prop("disabled",true);
 			}
@@ -226,6 +230,7 @@ function showFieldSettings(fullID) {
 				$("#fieldSettings_name").removeAttr("readonly");
 				$("#fieldSettings_options_required").removeAttr("disabled");
 				$("#fieldSettings_options_duplicates").removeAttr("disabled");
+				$("#fieldSettings_options_duplicatesForm").removeAttr("disabled");
 				$("#fieldSettings_options_readonly").removeAttr("disabled");
 				$("#fieldSettings_options_disabled").removeAttr("disabled");
 			}
@@ -327,6 +332,7 @@ function showFieldSettings(fullID) {
 
 			$("#fieldSettings_options_required").prop("checked",($("#required_"+id).val()==='true'));
 			$("#fieldSettings_options_duplicates").prop("checked",($("#duplicates_"+id).val()==='true'));
+			$("#fieldSettings_options_duplicatesForm").prop("checked",($("#duplicatesForm_"+id).val()==='true'));
 			$("#fieldSettings_options_readonly").prop("checked",($("#readonly_"+id).val()==='true')).change();
 			$("#fieldSettings_options_disabled").prop("checked",($("#disabled_"+id).val()==='true')).change();
 			$("#fieldSettings_options_publicRelease").prop("checked",($("#publicRelease_"+id).val()==='true')).change();
@@ -622,6 +628,12 @@ function fieldSettingsBindings() {
 
 	$("#fieldSettings_options_duplicates").change(function() {
 		$("#formPreview .well :input[name^=duplicates_]").val($(this).is(":checked"));
+		$("#fieldSettings_options_duplicatesForm").removeAttr("checked");
+	});
+
+	$("#fieldSettings_options_duplicatesForm").change(function() {
+		$("#formPreview .well :input[name^=duplicatesForm_]").val($(this).is(":checked"));
+		$("#fieldSettings_options_duplicates").removeAttr("checked");
 	});
 
 	$("#fieldSettings_options_readonly").change(function() {
@@ -1083,6 +1095,7 @@ function newFieldValues(id,type,vals) {
 	output += '<input type="hidden" id="style_'+id+'" name="style_'+id+'" value="'+((vals['style']!=undefined)?vals['style']:'')+'">';
 	output += '<input type="hidden" id="required_'+id+'" name="required_'+id+'" value="'+((vals['required']!=undefined)?vals['required']:'false')+'">';
 	output += '<input type="hidden" id="duplicates_'+id+'" name="duplicates_'+id+'" value="'+((vals['duplicates']!=undefined)?vals['duplicates']:'false')+'">';
+	output += '<input type="hidden" id="duplicatesForm_'+id+'" name="duplicatesForm_'+id+'" value="'+((vals['duplicatesForm']!=undefined)?vals['duplicatesForm']:'false')+'">';
 	output += '<input type="hidden" id="readonly_'+id+'" name="readonly_'+id+'" value="'+((vals['readonly']!=undefined)?vals['readonly']:'false')+'">';
 	output += '<input type="hidden" id="disabled_'+id+'" name="disabled_'+id+'" value="'+((vals['disabled']!=undefined)?vals['disabled']:'false')+'">';
 	output += '<input type="hidden" id="publicRelease_'+id+'" name="publicRelease_'+id+'" value="'+((vals['publicRelease']!=undefined)?vals['publicRelease']:'true')+'">';
