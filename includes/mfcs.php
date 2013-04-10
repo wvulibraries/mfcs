41b22d19b723c6a63209767b003d395f5bbfbc07
David Gersting <david.gersting@mail.wvu.edu>
2013-04-10 17:11:08 -0400

    Added user handling logic and user() method for retrieving  user fields

--- includes/mfcs.php
+++ includes/mfcs.php
@@ -27,6 +27,8 @@ class mfcs {
      */
     private static $config = array();
 
+    private static $user = array();
+
     /**
      * Class constructor
      *
@@ -37,9 +39,33 @@ class mfcs {
         self::$engine   = EngineAPI::singleton();
         self::$mfcsRoot = realpath(__DIR__.'/..');
 
+        // Load config file
         if(is_null($configFile)) $configFile = self::$mfcsRoot.'/config.ini';
         if(is_readable($configFile)){
             self::$config = parse_ini_file($configFile);
+        }
+
+        // Process the logged in user
+        $username  = sessionGet('username');
+        $sqlSelect = sprintf("SELECT * FROM users WHERE username='%s' LIMIT 1", self::$engine->openDB->escape($username));
+        $sqlResult = self::$engine->openDB->query($sqlSelect);
+        if(!$sqlResult['result']){
+            errorHandle::newError(__METHOD__."() - Failed to lookup user ({$sqlResult['error']})", errorHandle::HIGH);
+        }else{
+            if(!$sqlResult['numRows']){
+                // No user found, add them!
+                $sqlInsert = sprintf("INSERT INTO users (username) VALUES('%s')", self::$engine->openDB->escape($username));
+                $sqlResult = self::$engine->openDB->query($sqlInsert);
+                if(!$sqlResult['result']){
+                    errorHandle::newError(__METHOD__."() - Failed to insert new user ({$sqlResult['error']})", errorHandle::DEBUG);
+                }else{
+                    $sqlResult = self::$engine->openDB->query($sqlSelect);
+                    self::$user = mysql_fetch_assoc($sqlResult['result']);
+                }
+            }else{
+                self::$user = mysql_fetch_assoc($sqlResult['result']);
+            }
+
         }
     }
 
@@ -66,6 +92,20 @@ class mfcs {
     public static function config($name,$default=NULL){
         return isset(self::$config[$name])
             ? self::$config[$name]
+            : $default;
+    }
+
+    /**
+     * Get a user field
+     *
+     * @author David Gersting
+     * @param string $name The name of the user field
+     * @param mixed $default If no field value found, return this
+     * @return mixed
+     */
+    public static function user($name,$default=NULL){
+        return (isset(self::$user[$name]) and !empty(self::$user[$name]))
+            ? self::$user[$name]
             : $default;
     }
 
