8cb5c79dbc7e1a70f2d141a05e69d199e061182b
MichaelRBond <mbond@the-forgotten.org>
2013-04-10 13:39:32 -0400

    changed from version number to latest

--- engineInclude.php
+++ engineInclude.php
@@ -1,5 +1,5 @@
 <?php
-require_once("/home/dev1.systems.lib.wvu.edu/phpincludes/engine/engineAPI/3.2/engine.php");
+require_once("/home/dev1.systems.lib.wvu.edu/phpincludes/engine/engineAPI/latest/engine.php");
 $engine = EngineAPI::singleton();
 
 // Load the mfcs class
