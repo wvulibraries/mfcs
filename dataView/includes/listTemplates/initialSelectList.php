a7147bbdd4af37f82e717ee214d067905347fac4
MichaelRBond <mbond@the-forgotten.org>
2013-04-11 11:45:16 -0400

    object selection -- pick starting point

---
+++ dataView/includes/listTemplates/initialSelectList.php
@@ -0,0 +1,11 @@
+<ul>
+	<li>
+		<a href="{phpself query="false"}?listType=all">All Objects</a>
+	</li>
+	<li>
+		<a href="{phpself query="false"}?listType=selectForm">Objects by Form</a>
+	</li>
+	<li>
+		<a href="{phpself query="false"}?listType=selectProject">Objects by Project</a>
+	</li>
+</ul>
