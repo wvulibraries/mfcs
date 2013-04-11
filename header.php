0816062a847bb4818cf3c93c39c67f3059b8e470
MichaelRBond <mbond@the-forgotten.org>
2013-04-11 08:32:48 -0400

    moved modal out of header into topnav, so that it is in the body instead of outside of the HTML tags

--- header.php
+++ header.php
@@ -40,31 +40,3 @@ localVars::add("siteRoot",$engineVars['WEBROOT']."/mfcs/");
 localVars::add('pageTitle',"Metadata Form Creation System");
 localVars::add('pageHeader',"Metadata Form Creation System");
 ?>
-
-<!-- Modal - Select Current Projects -->
-<!-- <div id="selectProjects" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h3 id="myModalLabel">Your current projects:</h3>
-    </div>
-    <div class="modal-body">
-        <ul id="selectProjectsList"> -->
-            <?php
-
-            // @TODO : This logic should be moved out of the HTML and put into a local variable
-
-            // $currentProjectsIDs = array_keys($currentProjects);
-            // foreach(projects::getProjects() as $project){
-            //     echo sprintf("<li><label><input type='checkbox' value='%s'%s> %s</label></li>",
-            //         $project['ID'],
-            //         in_array($project['ID'], $currentProjectsIDs) ? " checked='checked'" : '',
-            //         $project['projectName']);
-            // }
-            ?>
- <!--       </ul>
-    </div>
-    <div class="modal-footer">
-        <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
-        <button class="btn btn-primary" onclick="saveSelectedProjects();">Save changes</button>
-    </div>
-</div> -->
