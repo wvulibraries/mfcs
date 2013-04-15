9d19b1296c71bda720408879429671acd1f87c8d
MichaelRBond <mbond@the-forgotten.org>
2013-04-15 09:51:42 -0400

    switch to using classes for ID validation on objects and forms

--- dataEntry/object.php
+++ dataEntry/object.php
@@ -4,45 +4,19 @@ recurseInsert("acl.php","php");
 
 try {
 
-	$error = FALSE;
+	$error = FALSE; 
 
-	if (!isset($engine->cleanGet['MYSQL']['formID'])
-		|| is_empty($engine->cleanGet['MYSQL']['formID'])
-		|| !validate::integer($engine->cleanGet['MYSQL']['formID'])) {
-
-		if (!isnull($engine->cleanGet['MYSQL']['objectID'])) {
-			$object = objects::get($engine->cleanGet['MYSQL']['objectID']);
-
-			if ($object === FALSE) {
-				errorHandle::newError(__METHOD__."() - No Form ID Provided, error getting Object", errorHandle::DEBUG);
-				throw new Exception("No Form ID Provided, error getting Object.");
-			}
-
-			http::setGet('formID',$object['formID']);
-
-		}
-		else {
-			errorHandle::newError(__METHOD__."() - No Form ID Provided.", errorHandle::DEBUG);
-			throw new Exception("No Form ID Provided.");
-		}
+	if (objects::validID() === FALSE) {
+		throw new Exception("ObjectID Provided is invalid.");
 	}
 
-	if (isset($engine->cleanGet['MYSQL']['objectID'])) {
-		if (is_empty($engine->cleanGet['MYSQL']['objectID'])
-			|| !validate::integer($engine->cleanGet['MYSQL']['objectID'])) {
-
-			errorHandle::newError("ObjectID Provided is invalid", errorHandle::DEBUG);
-			throw new Exception("ObjectID Provided is invalid.");
-		}
-
-		// if an object ID is provided make sure the object is from this form
-		if (!objects::checkObjectInForm($engine->cleanGet['MYSQL']['formID'],$engine->cleanGet['MYSQL']['objectID'])) {
-			errorHandle::newError("Object not from this form.", errorHandle::DEBUG);
-			throw new Exception("Object not from this form.");
-		}
+	if (forms::validID() === FALSE) {
+		throw new Exception("No Form ID Provided.");
 	}
-	else if (!isset($engine->cleanGet['MYSQL']['objectID'])) {
-		$engine->cleanGet['MYSQL']['objectID'] = NULL;
+
+	// if an object ID is provided make sure the object is from this form
+	if (!isnull($engine->cleanGet['MYSQL']['objectID']) && !objects::checkObjectInForm($engine->cleanGet['MYSQL']['formID'],$engine->cleanGet['MYSQL']['objectID'])) {
+		throw new Exception("Object not from this form.");
 	}
 
 	$form = forms::get($engine->cleanGet['MYSQL']['formID']);
@@ -96,12 +70,20 @@ catch(Exception $e) {
 }
 
 // build the form for displaying
-$builtForm = forms::build($engine->cleanGet['MYSQL']['formID'],$engine->cleanGet['MYSQL']['objectID'],$error);
-if ($builtForm === FALSE) {
-	throw new Exception("Error building form.");
+if (forms::validID()) {
+	try {
+		$builtForm = forms::build($engine->cleanGet['MYSQL']['formID'],$engine->cleanGet['MYSQL']['objectID'],$error);
+		if ($builtForm === FALSE) {
+			throw new Exception("Error building form.");
+		}
+		localvars::add("form",$builtForm);
+		localvars::add("leftnav",buildProjectNavigation($engine->cleanGet['MYSQL']['formID']));
+	}
+	catch (Exception $e) {
+		errorHandle::errorMsg($e->getMessage());
+	}
 }
-localvars::add("form",$builtForm);
-localvars::add("leftnav",buildProjectNavigation($engine->cleanGet['MYSQL']['formID']));
+
 localVars::add("results",displayMessages());
 
 $engine->eTemplate("include","header");
--- dataView/object.php
+++ dataView/object.php
@@ -5,50 +5,11 @@ recurseInsert("acl.php","php");
 try {
 
 	// Object ID Validation
-	if (!isset($engine->cleanGet['MYSQL']['objectID'])) {
-		errorHandle::newError("No Object ID Provided.", errorHandle::DEBUG);
-		throw new Exception("No Object ID Provided.");
+	if (objects::validID(TRUE) === FALSE) {
+		throw new Exception("ObjectID Provided is invalid.");
 	}
 
-	if (is_empty($engine->cleanGet['MYSQL']['objectID'])
-		|| !validate::integer($engine->cleanGet['MYSQL']['objectID'])) {
-
-		errorHandle::newError("Object ID Provided is invalid.", errorHandle::DEBUG);
-		throw new Exception("Object ID Provided is invalid.");
-	}
-
-	// check for edit permissions on the project
-	// if (projects::checkPermissions($engine->cleanGet['MYSQL']['id']) === FALSE) {
-	// 	throw new Exception("Permissions denied for working on this project");
-	// }
-
-	// check that this form is part of the project
-	// // TODO need forms from User
-	// if (!forms::checkFormInProject($engine->cleanGet['MYSQL']['id'],$engine->cleanGet['MYSQL']['formID'])) {
-	// 	throw new Exception("Form is not part of project.");
-	// }
-
-	// Get the project
-	// $project = NULL; // TODO: Needs to be gotten from the user info
-	// if ($project === FALSE) {
-	// 	throw new Exception("Error retrieving project.");
-	// }
-
-
-	$object = objects::get($engine->cleanGet['MYSQL']['objectID']);
-	if ($object === FALSE) {
-		throw new Exception("Invalid Object ID.");
-	}
-
-	http::setGet('formID',$object['formID']);
-
-
-	// Form ID Validation
-	if (!isset($engine->cleanGet['MYSQL']['formID'])
-		|| is_empty($engine->cleanGet['MYSQL']['formID'])
-		|| !validate::integer($engine->cleanGet['MYSQL']['formID'])) {
-
-		errorHandle::newError("No Form ID Provided.", errorHandle::DEBUG);
+	if (forms::validID() === FALSE) {
 		throw new Exception("No Form ID Provided.");
 	}
 
@@ -65,21 +26,6 @@ try {
 	}
 
 	localvars::add("formName",$form['title']);
-
-	// Submissions
-	if (isset($engine->cleanPost['MYSQL']['submitForm'])) {
-		$return = forms::submit($engine->cleanGet['MYSQL']['formID']);
-		if ($return === FALSE) {
-			throw new Exception("Error Submitting Form.");
-		}
-	}
-	else if (isset($engine->cleanPost['MYSQL']['updateForm'])) {
-		$return = forms::submit($engine->cleanGet['MYSQL']['formID'],$engine->cleanGet['MYSQL']['objectID']);
-		if ($return === FALSE) {
-			throw new Exception("Error Updating Form.");
-		}
-	}
-	// Submissions
 
 	// build the form for displaying
 	$builtForm = forms::build($engine->cleanGet['MYSQL']['formID'],$engine->cleanGet['MYSQL']['objectID']);
