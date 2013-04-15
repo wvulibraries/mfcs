d137742b9a44c90e544b1d03900d389caf3c3ac0
MichaelRBond <mbond@the-forgotten.org>
2013-04-15 13:34:34 -0400

    pull current projects from users class()

--- header.php
+++ header.php
@@ -8,26 +8,16 @@ $engine->dbConnect("database","mfcs",TRUE);
 
 // Load the mfcs class
 require_once "includes/mfcs.php";
-require_once "includes/projects.php";
 require_once "includes/forms.php";
-require_once "includes/objects.php";
 require_once "includes/list.php";
+require_once "includes/objects.php";
+require_once "includes/projects.php";
+require_once "includes/users.php";
 mfcs::singleton();
 
 // Load the user's current projects
-$currentProjects = array();
-$sql = sprintf("SELECT projects.ID,projectName FROM `projects` LEFT JOIN users_projects ON users_projects.projectID=projects.ID WHERE users_projects.userID=%s",
-mfcs::$engine->openDB->escape(mfcs::user('ID')));
-$sqlResult = $engine->openDB->query($sql);
-if(!$sqlResult['result']){
-    errorHandle::newError("Failed to load user's projects ({$sqlResult['error']})", errorHandle::HIGH);
-    errorHandle::errorMsg("Failed to load your current projects.");
-}else{
-    while($row = mysql_fetch_assoc($sqlResult['result'])){
-        $currentProjects[ $row['ID'] ] = $row['projectName'];
-    }
-}
-sessionSet('currentProject',$currentProjects);
+
+sessionSet('currentProject',users::loadProjects());
 
 recurseInsert("includes/functions.php","php");
 recurseInsert("includes/validator.php","php");
