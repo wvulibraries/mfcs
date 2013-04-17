c4d28779b59ba077bed99f5782702b1283de6599
MichaelRBond <mbond@the-forgotten.org>
2013-04-17 10:49:16 -0400

    modified to use a try/catch block. displays error message in list on error instead of a blank list.

--- footerIncludes.php
+++ footerIncludes.php
@@ -1,13 +1,24 @@
 <?php
 $currentProjectsIDs = array_keys(sessionGet('currentProject'));
 $projectListHTML = '<ul id="selectProjectsList">';
-foreach(projects::getProjects() as $project){
-    $projectListHTML .= sprintf("<li><label><input type='checkbox' value='%s' data-label='%s'%s> %s</label></li>",
-        $project['ID'],
-        $project['projectName'],
-        in_array($project['ID'], $currentProjectsIDs) ? " checked='checked'" : '',
-        $project['projectName']);
+
+try {
+	if (($projects = projects::getProjects()) === FALSE) {
+		throw new Exception("Error retrieving project list.");
+	}
+
+	foreach($projects as $project){
+		$projectListHTML .= sprintf("<li><label><input type='checkbox' value='%s' data-label='%s'%s> %s</label></li>",
+			$project['ID'],
+			$project['projectName'],
+			in_array($project['ID'], $currentProjectsIDs) ? " checked='checked'" : '',
+			$project['projectName']);
+	}
 }
+catch(Exception $e) {
+	$projectListHTML .= "<li>".$e->getMessage()."</li>";
+}
+
 $projectListHTML .= '</ul>';
 localvars::add('projectModalList', $projectListHTML);
 ?>
