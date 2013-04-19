becbb57d8594e0228c91153e0793dbe28ec92680
MichaelRBond <mbond@the-forgotten.org>
2013-04-19 10:30:48 -0400

    removed checkPermissions() method

--- includes/projects.php
+++ includes/projects.php
@@ -87,43 +87,6 @@ class projects {
 
     }
 
-    /**
-     * returns the database object for the project ID. If no projectID is provided, 
-     * returns an array of all the projects, using getProject method defaults
-     * we need to add caching to this, once caching is moved from EngineCMS to EngineAPI
-     *
-     * @author Michael Bond
-     * @param integer $id MySQL ID of the project to check permissions
-     * @param  string $username If provided checks against $username, otherwise current logged in user
-     * @return boolean
-     */
-    public static function checkPermissions($id,$username = NULL) {
-
-    	if (isnull($username)) {
-    		$username = sessionGet("username");
-    	}
-
-    	$sql       = sprintf("SELECT COUNT(permissions.ID) FROM permissions LEFT JOIN users on users.ID=permissions.userID WHERE permissions.projectID='%s' AND users.username='%s'",
-            mfcs::$engine->openDB->escape($id),
-            mfcs::$engine->openDB->escape($username)
-    		);
-    	$sqlResult = mfcs::$engine->openDB->query($sql);
-
-    	if (!$sqlResult['result']) {
-    		errorHandle::newError(__METHOD__."() - ".$sqlResult['error'], errorHandle::DEBUG);
-    		return FALSE;
-    	}
-
-    	$row       = mysql_fetch_array($sqlResult['result'],  MYSQL_ASSOC);
-
-    	if ((int)$row['COUNT(permissions.ID)'] > 0) {
-    		return TRUE;
-    	}
-
-    	return FALSE;
-
-    }
-
     // $selected is an array of projectIDs
     public static function generateProjectCheckList($selected=array()) {
 
