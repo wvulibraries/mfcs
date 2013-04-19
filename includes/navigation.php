e66777fc26a3f7d614d026bd0f201745b2b59500
MichaelRBond <mbond@the-forgotten.org>
2013-04-19 11:26:28 -0400

    added buildProjectNavigation() Method

--- includes/navigation.php
+++ includes/navigation.php
@@ -31,6 +31,63 @@ class navigation {
 
 	}
 
+public static function buildProjectNavigation($formID) {
+	
+	if (($form = forms::get($formID)) === FALSE) {
+		return(FALSE);
+	}
+
+	$output       = "";
+	$currentGroup = "";
+
+	foreach ($form['navigation'] as $item) {
+
+		// deal with field sets
+		if ($item['grouping'] != $currentGroup) {
+			if ($currentGroup != "") {
+				$output .= "</ul></li>";
+			}
+			if (!isempty($item['grouping'])) {
+				$output .= sprintf('<li><strong>%s</strong><ul>',
+					$item['grouping']
+				);
+			}
+			$currentGroup = $item['grouping'];
+		}
+
+		$output .= "<li>";
+		if ($item['type'] == "logout") {
+			$output .= sprintf('<a href="%s">%s</a>',
+				htmlSanitize($item['url']),
+				htmlSanitize($item['label'])
+			);
+		}
+		else if ($item['type'] == "link") {
+			$output .= sprintf('<a href="%s">%s</a>',
+				htmlSanitize($item['url']),
+				htmlSanitize($item['label'])
+			);
+		}
+		else if ($item['type'] == "objectForm" || $item['type'] == "metadataForm") {
+			$output .= sprintf('<a href="%s.php?formID=%s">%s</a>',
+				($item['type'] == "objectForm")?"object":"metadata",
+				htmlSanitize($item['formID']),
+				htmlSanitize($item['label'])
+			);
+		}
+		else {
+			$output .= sprintf('%s',
+				htmlSanitize($item['label'])
+			);
+		}
+		$output .= "</li>";
+
+	}
+
+
+	return $output;
+}
+
 }
 
 ?>
