14777a4ea31251ad63a8fa9a51d74de6552a692e
MichaelRBond <mbond@the-forgotten.org>
2013-04-18 21:06:06 -0400

    added methods delete(), add()

--- includes/permissions.php
+++ includes/permissions.php
@@ -14,6 +14,49 @@ class mfcsPerms {
 
 	}
 
+	public static function add($userID,$formID,$type) {
+
+		$sql       = sprintf("INSERT INTO `permissions` (userID,formID,type) VALUES('%s','%s','%s')",
+			mfcs::$engine->openDB->escape($userID),
+			mfcs::$engine->openDB->escape($formID),
+			mfcs::$engine->openDB->escape($type)
+			);
+		$sqlResult = mfcs::$engine->openDB->query($sql);
+		
+		if (!$sqlResult['result']) {
+			errorHandle::newError(__METHOD__."() - : ".$sqlResult['error'], errorHandle::DEBUG);
+			return FALSE;
+		}
+		
+		return TRUE;
+
+	}
+
+	public static function delete($id = NULL) {
+
+		if (!isnull($id)) {
+			$whereClause = sprintf("WHERE `formID`='%s'",
+				mfcs::$engine->openDB->escape($id)
+				);
+		}
+		else {
+			$whereClause = "";
+		}
+
+		$sql       = sprintf("DELETE FROM `permissions` %s",
+			$whereClause
+			);
+		$sqlResult = mfcs::$engine->openDB->query($sql);
+		
+		if (!$sqlResult['result']) {
+			errorHandle::newError(__METHOD__."() - : ".$sqlResult['error'], errorHandle::DEBUG);
+			return FALSE;
+		}
+		
+		return TRUE;
+
+	}
+
 }
 
 ?>
