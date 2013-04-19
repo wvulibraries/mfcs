368cbe65d2a61a30c31c478cc4bfe52f29575f57
MichaelRBond <mbond@the-forgotten.org>
2013-04-19 16:44:13 -0400

    modal for metadata

--- includes/js/mfcs.js
+++ includes/js/mfcs.js
@@ -26,17 +26,43 @@ $(function(){
     });
 
     $(document)
-        .on('click', '.metadataObjectEditor', handler_displayMetadataFormModal)
+        .on('click', '.metadataObjectEditor', handler_setupMetadataModal)
 
 
 });
 
-function handler_displayMetadataFormModal() {
+function handler_setupMetadataModal() {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    $("#metadataModal .modal-header h3").html($(this).attr("data-header"));
+
+    var dataFieldName = $(this).attr("data-fieldname");
+    var url           = siteRoot+'/dataEntry/metadata.php?formID='+$(this).attr('data-formID')+'&amp;ajax=true';
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "html",
+        success: function(responseData) {
+            $("#metadataModalBody").html(responseData);
+
+             $("#metadataModalBody :submit").remove();
+             $("#metadataModalBody header").remove();
+             $("#metadataModalBody footer").remove();
+        },
+        error: function(jqXHR,error,exception) {
+            $('#metadataModalBody').html("An Error has occurred: "+error);
+        }
+    });
+
+}
+
+function handler_displayMetadataFormModal(formID) {
 
     event.preventDefault();
     event.stopImmediatePropagation();
 
-    var choicesForm = $(this).attr("data-name");
+    var choicesForm = formID;//$(this).attr("data-formID");
 
     $("[data-choicesForm='"+choicesForm+"']").each(function() {
         var dataFieldName = $(this).attr("data-fieldname");
@@ -47,7 +73,6 @@ function handler_displayMetadataFormModal() {
             url: url,
             dataType: "html",
             success: function(responseData) {
-                console.log(responseData);
                 $("[data-fieldname='"+dataFieldName+"']").html(responseData);
             },
             error: function(jqXHR,error,exception) {
@@ -55,6 +80,40 @@ function handler_displayMetadataFormModal() {
         }
         });
     });
+
+}
+
+function submitMetadataModal() {
+
+    $("#metadataModalBody form").each(function() {
+
+        data = $(this).serialize();
+
+        if ($(this).attr("name") == "insertForm") {
+            data = data + "&submitForm=TRUE"
+        }
+        else if ($(this).attr("name") == "updateForm") {
+            data = data + "&updateEdit=Update";
+        }
+
+        $.ajax({
+            type: "POST",
+            url: $(this).attr("action")+"&ajax=true",
+            dataType: "html",
+            data: data,
+            async:   false,
+            success: function(responseData) {
+                console.log(responseData);
+            },
+            error: function(jqXHR,error,exception) {
+                $("#metadataModalBody").html("An Error has occurred: "+error);
+            }
+        });
+
+    });
+
+    $('#metadataModal').modal('hide');
+    handler_displayMetadataFormModal($(this).attr("data-formid"));
 
 }
 
