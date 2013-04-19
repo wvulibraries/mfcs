70f8ab90ea63b7ccb3ae01de3fef6e2600f91645
David Gersting <david.gersting@mail.wvu.edu>
2013-04-19 14:05:00 -0400

    Added dynamic breadcrumbs depending on the state of list.php

--- dataView/list.php
+++ dataView/list.php
@@ -6,21 +6,43 @@ try {
 
 	if (!isset($engine->cleanGet['MYSQL'])) $engine->cleanGet['MYSQL'] = array("listType" => "");
 
+	// Setup the start of the breadcrumbs and pre-populate what we can
+	$siteRoot = localvars::get('siteRoot');
+	$breadCrumbs = array(
+		sprintf('<a href="%s">Home</a>', $siteRoot),
+		sprintf('<a href="%s/dataView/list.php">List Objects</a>', $siteRoot)
+	);
+
+	// Figure out what kind of list we're building
 	switch($engine->cleanGet['MYSQL']['listType']) {
 		case 'selectForm':
 			$list = listGenerator::createFormSelectList();
+			localvars::add('subTitle',' - Select Form');
+			$breadCrumbs[] = sprintf('<a href="%s/dataView/list.php?listType=selectForm">Select Form</a>', $siteRoot);
 			break;
 		case 'selectProject':
 			$list = listGenerator::createProjectSelectList();
+			localvars::add('subTitle',' - Select Project');
+			$breadCrumbs[] = sprintf('<a href="%s/dataView/list.php?listType=selectProject">Select Project</a>', $siteRoot);
 			break;
 		case 'form':
 			$list = listGenerator::createFormObjectList($engine->cleanGet['MYSQL']['formID']);
+			$form = forms::get($engine->cleanGet['MYSQL']['formID']);
+			localvars::add('subTitle',' - '.$form['title']);
+			$breadCrumbs[] = sprintf('<a href="%s/dataView/list.php?listType=selectForm">Select Form</a>', $siteRoot);
+			$breadCrumbs[] = sprintf('<a href="%s/dataView/list.php?listType=form&formID=%s">%s</a>', $siteRoot, $form['ID'], $form['title']);
 			break;
 		case 'project':
-			$list = listGenerator::createProjectObjectList($engine->cleanGet['MYSQL']['projectID']);
+			$list    = listGenerator::createProjectObjectList($engine->cleanGet['MYSQL']['projectID']);
+			$project = projects::get($engine->cleanGet['MYSQL']['projectID']);
+			localvars::add('subTitle',' - '.$project['projectName']);
+			$breadCrumbs[] = sprintf('<a href="%s/dataView/list.php?listType=selectProject">Select Project</a>', $siteRoot);
+			$breadCrumbs[] = sprintf('<a href="%s/dataView/list.php?listType=project&projectID=%s">%s</a>', $siteRoot, $project['ID'], $project['projectName']);
 			break;
 		case 'all':
 			$list = listGenerator::createAllObjectList();
+			localvars::add('subTitle',' - All Objects');
+			$breadCrumbs[] = sprintf('<a href="%s/dataView/list.php?listType=all">All Objects</a>', $siteRoot);
 			break;
 		default:
 			$list = listGenerator::createInitialSelectList();
@@ -28,6 +50,7 @@ try {
 	}
 
 	localvars::add("list",$list);
+	localvars::add("breadcrumbs", implode('<span class="divider">/</span>', $breadCrumbs));
 
 }
 catch(Exception $e) {
@@ -40,14 +63,12 @@ $engine->eTemplate("include","header");
 
 <section>
 	<header class="page-header">
-		<h1>Listing Objects</h1>
+		<h1>List Objects{local var="subTitle"}</h1>
 	</header>
 	<nav id="breadcrumbs">
 		<ul class="breadcrumb">
 			<li>
-				<a href="{local var="siteRoot"}">Home</a>
-				<span class="divider">/</span>
-				<a href="{local var="siteRoot"}/dataView/list.php">List Objects</a>
+				{local var="breadcrumbs"}
 			</li>
 		</ul>
 	</nav>
