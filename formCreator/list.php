e3f571731c307e26ac735d19db7219d097de4c49
David Gersting <david.gersting@mail.wvu.edu>
2013-04-19 14:10:12 -0400

    Added breadcrumbs

--- formCreator/list.php
+++ formCreator/list.php
@@ -23,6 +23,13 @@ $engine->eTemplate("include","header");
 	<header class="page-header">
 		<h1>Select a Form</h1>
 	</header>
+	<nav id="breadcrumbs">
+		<ul class="breadcrumb">
+			<li>
+				<a href="{local var="siteRoot"}">Home</a><span class="divider">/</span><a href="{local var="siteRoot"}/formCreator/list.php">List Forms</a>
+			</li>
+		</ul>
+	</nav>
 
 	{local var="results"}
 
