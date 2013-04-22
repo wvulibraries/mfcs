b394cf8b12a25fbafe1ec77a0b2dd2f9866348b5
David Gersting <david.gersting@mail.wvu.edu>
2013-04-22 13:37:32 -0400

    Added isAdmin permission check for Edit Form() link

--- dataEntry/object.php
+++ dataEntry/object.php
@@ -168,8 +168,11 @@ $engine->eTemplate("include","header");
 			<li><a href="{local var="siteRoot"}">Home</a> <span class="divider">/</span></li>
 			<li><a href="{local var="siteRoot"}/dataEntry/selectForm.php">Select a Form</a> <span class="divider">/</span></li>
 			<!-- FLoat Right -->
+			<?php if(mfcsPerms::isAdmin($engine->cleanGet['MYSQL']['formID'])){ ?>
 			<li class="pull-right"><a href="{local var="siteRoot"}formCreator/index.php?id={local var="formID"}">Edit Form</a></li>
-			<?php if (!isnull($engine->cleanGet['MYSQL']['objectID']) and $revisions->hasRevisions($engine->cleanGet['MYSQL']['objectID'])) { ?>
+			<?php
+			}
+			if (!isnull($engine->cleanGet['MYSQL']['objectID']) and $revisions->hasRevisions($engine->cleanGet['MYSQL']['objectID'])) { ?>
 				<li class="pull-right"><a href="{local var="siteRoot"}dataEntry/revisions.php?objectID={local var="objectID"}">Revisions</a></li>
 			<?php } ?>			
 		</ul>
