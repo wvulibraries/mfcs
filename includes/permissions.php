eecde9d69c56c86cb79f6f7c3a0d8b89397d15d6
David Gersting <david.gersting@mail.wvu.edu>
2013-04-22 14:02:49 -0400

    Code cleanup

--- includes/permissions.php
+++ includes/permissions.php
@@ -3,11 +3,9 @@
 class mfcsPerms {
 
 	private static function getCount($formID,$username,$type) {
-
-		$sql       = sprintf("SELECT COUNT(permissions.ID) FROM permissions LEFT JOIN users on users.ID=permissions.userID WHERE permissions.formID='%s' AND users.username='%s'",
+		$sql = sprintf("SELECT COUNT(permissions.ID) FROM permissions LEFT JOIN users on users.ID=permissions.userID WHERE permissions.formID='%s' AND users.username='%s'",
             mfcs::$engine->openDB->escape($formID),
-            mfcs::$engine->openDB->escape($username)
-    		);
+            mfcs::$engine->openDB->escape($username));
     	$sqlResult = mfcs::$engine->openDB->query($sql);
 
     	if (!$sqlResult['result']) {
@@ -16,44 +14,30 @@ class mfcsPerms {
     	}
 
     	$row = mysql_fetch_array($sqlResult['result'],  MYSQL_ASSOC);
-
-    	if ((int)$row['COUNT(permissions.ID)'] > 0) {
-    		return TRUE;
-    	}
-    	else {
-    		return FALSE;
-    	}
-
-    	return FALSE;
-
+    	if ((int)$row['COUNT(permissions.ID)'] > 0) return TRUE;
+		return FALSE;
 	}
 
 	public static function isAdmin($formID, $username = NULL) {
 		if (isnull($username)) $username = sessionGet("username");
-
 		return self::getCount($formID,$username,mfcs::AUTH_ADMIN) || trim(strtolower(users::user('status','user'))) == 'systems';
 	}
 
 	public static function isEditor($formID, $username = NULL) {
 		if (isnull($username)) $username = sessionGet("username");
-
 		return self::isAdmin($formID,$username) || self::getCount($formID,$username,mfcs::AUTH_ENTRY);
 	}
 
 	public static function isViewer($formID, $username = NULL) {
-
 		if (isnull($username)) $username = sessionGet("username");
-
 		return self::isAdmin($formID,$username) || self::isEditor($formID,$username) || self::getCount($formID,$username,mfcs::AUTH_VIEW);
 	}
 
 	public static function add($userID,$formID,$type) {
-
-		$sql       = sprintf("INSERT INTO `permissions` (userID,formID,type) VALUES('%s','%s','%s')",
+		$sql = sprintf("INSERT INTO `permissions` (userID,formID,type) VALUES('%s','%s','%s')",
 			mfcs::$engine->openDB->escape($userID),
 			mfcs::$engine->openDB->escape($formID),
-			mfcs::$engine->openDB->escape($type)
-			);
+			mfcs::$engine->openDB->escape($type));
 		$sqlResult = mfcs::$engine->openDB->query($sql);
 		
 		if (!$sqlResult['result']) {
@@ -62,14 +46,11 @@ class mfcsPerms {
 		}
 		
 		return TRUE;
-
 	}
 
 	public static function delete($id) {
-
-		$sql       = sprintf("DELETE FROM `permissions` WHERE `formID`='%s'",
-			mfcs::$engine->openDB->escape($id)
-			);
+		$sql = sprintf("DELETE FROM `permissions` WHERE `formID`='%s'",
+			mfcs::$engine->openDB->escape($id));
 		$sqlResult = mfcs::$engine->openDB->query($sql);
 		
 		if (!$sqlResult['result']) {
@@ -78,9 +59,7 @@ class mfcsPerms {
 		}
 		
 		return TRUE;
-
 	}
-
 }
 
 ?>
