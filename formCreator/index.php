28326f086383f01ab1233e7ed1323f266f437bb8
MichaelRBond <mbond@the-forgotten.org>
2013-04-23 14:51:00 -0400

    todo

--- formCreator/index.php
+++ formCreator/index.php
@@ -404,6 +404,7 @@ if (!isnull($formID)) {
 		}
 
 		// Get existing groupings
+		// @TODO -- cleanGet isn't always set if formID isn't null, but formID is html safe not mysql safe. 
 		$sql = sprintf("SELECT * FROM `forms` WHERE `ID`='%s' LIMIT 1",
 			$engine->cleanGet['MYSQL']['id']
 		);
