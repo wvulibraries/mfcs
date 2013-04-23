ba5cba5c763393f5c8a7bd4b1fafe819f741d03e
David Gersting <david.gersting@mail.wvu.edu>
2013-04-23 09:44:46 -0400

    Added basic markup for the stats page

--- stats/index.php
+++ stats/index.php
@@ -1,8 +1,21 @@
 <?php
 include("../header.php");
+$engine->eTemplate("include","header");
 
 $stats = new mfcsStats(".");
-
-$stats->showStatFiles();
-
 ?>
+<section>
+	<header class="page-header">
+		<h1>System Stats</h1>
+	</header>
+
+	<nav id="breadcrumbs">
+		<ul class="breadcrumb">
+			<li><a href="{local var="siteRoot"}">Home</a> <span class="divider">/</span></li>
+			<li><a href="{local var="siteRoot"}/stats/">Stats</a> <span class="divider">/</span></li>
+		</ul>
+	</nav>
+
+	<?php $stats->showStatFiles(); ?>
+
+</section>
