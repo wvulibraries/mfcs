c2e9c47b6b56d1786ec1b59fb340a65c8f934f33
David Gersting <david.gersting@mail.wvu.edu>
2013-04-24 11:47:14 -0400

    Bug fix: JavaScript crashes when a space is put into the Field Name field

--- includes/js/createForm_form.js
+++ includes/js/createForm_form.js
@@ -409,8 +409,8 @@ function fieldSettingsBindings() {
 	});
 
 	$("#fieldSettings_name").keyup(function() {
-		if ($("#formSettings_objectTitleField option[value="+$("#formPreview .well :input[name^=name_]").val()+"]").length > 0) {
-			$("#formSettings_objectTitleField option[value="+$("#formPreview .well :input[name^=name_]").val()+"]").val($(this).val());
+		if ($("#formSettings_objectTitleField option[value='"+$("#formPreview .well :input[name^=name_]").val()+"']").length > 0) {
+			$("#formSettings_objectTitleField option[value='"+$("#formPreview .well :input[name^=name_]").val()+"']").val($(this).val());
 		}
 		else if ($("#formPreview .well :input[name^=type_][value=text]").length > 0) {
 			$("#formSettings_objectTitleField").append('<option value="'+$(this).val()+'">'+$("#formPreview .well :input[name^=label_]").val()+'</option>');
@@ -421,8 +421,8 @@ function fieldSettingsBindings() {
 	});
 
 	$("#fieldSettings_label").keyup(function() {
-		if ($("#formSettings_objectTitleField option[value="+$("#formPreview .well :input[name^=name_]").val()+"]").length > 0) {
-			$("#formSettings_objectTitleField option[value="+$("#formPreview .well :input[name^=name_]").val()+"]").text($(this).val());
+		if ($("#formSettings_objectTitleField option[value='"+$("#formPreview .well :input[name^=name_]").val()+"']").length > 0) {
+			$("#formSettings_objectTitleField option[value='"+$("#formPreview .well :input[name^=name_]").val()+"']").text($(this).val());
 		}
 		else if ($("#formPreview .well :input[name^=type_][value=text]").length > 0) {
 			$("#formSettings_objectTitleField").append('<option value="'+$("#formPreview .well :input[name^=name_]").val()+'">'+$(this).val()+'</option>');
