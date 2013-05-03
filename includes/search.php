45bde34043cd90c7e31612b479732292765325f0
MichaelRBond <mbond@the-forgotten.org>
2013-05-03 14:21:40 -0400

    Display search Template

--- includes/search.php
+++ includes/search.php
@@ -2,6 +2,14 @@
 
 class mfcsSearch {
 
+	public static function buildInterface() {
+
+		$searchInterface = file_get_contents("../includes/templates/searchInterfaceTemplate.html");
+
+		return ($searchInterface);
+
+	}
+
 }
 
 ?>
