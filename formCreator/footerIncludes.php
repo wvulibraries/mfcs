08e0568aa20ff2df5ff1da899bc788c8534a9bf4
Scott Blake <Scott.Blake@mail.wvu.edu>
2013-05-03 08:56:51 -0400

    Remove "Loading..." modal

--- formCreator/footerIncludes.php
+++ formCreator/footerIncludes.php
@@ -1,22 +1,3 @@
-<div id="progressModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="progressBarModalBox" aria-hidden="true">
-	<div class="modal-header">
-		<h3 id="metadataModalHeader">Loading ...</h3>
-	</div>
-	<div class="modal-body">
-		<div class="progress progress-striped active">
-			<div id="createFormProgressBar" class="bar" style="width: 75%;"></div>
-		</div>
-	</div>
-	<div class="modal-footer">
-	</div>
-</div>
-
-<script type="text/javascript">
-	var pattern  = /id=/i;
-	var queryStr = location.search;
-	if(pattern.test(queryStr)) $('#progressModal').modal('show');
-</script>
-
 <script type="text/javascript" src='{local var="siteRoot"}includes/js/createForm_nav.js'></script>
 <script type="text/javascript" src='{local var="siteRoot"}includes/js/createForm_form.js'></script>
 <script type="text/javascript" src='{local var="siteRoot"}includes/js/createForm_permissions.js'></script>
