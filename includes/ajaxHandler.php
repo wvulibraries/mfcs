bd0ad29c7ac062287d0c2953ec8938448e7edf85
MichaelRBond <mbond@the-forgotten.org>
2013-05-08 11:11:10 -0400

    removed logic for updating user projects and calls users::updateUserProjects() instead

--- includes/ajaxHandler.php
+++ includes/ajaxHandler.php
@@ -5,53 +5,10 @@ if(isset($engine->cleanGet['MYSQL']['ajax'])){
     if (isset($engine->cleanPost['MYSQL']['action'])) {
         switch($engine->cleanPost['MYSQL']['action']){
             case 'updateUserProjects':
-            // @TODO this case statement should be broken off into another file or class
-            $currentProjectsIDs   = array_keys(sessionGet('currentProject'));
-            $submittedProjectsIDs = isset($engine->cleanPost['MYSQL']['selectedProjects'])
-            ? $engine->cleanPost['MYSQL']['selectedProjects']
-            : array();
-
-            try{
-                // Delete project IDs that disappeared
-                $deletedIDs = array_diff($currentProjectsIDs,$submittedProjectsIDs);
-                if(sizeof($deletedIDs)){
-                    $deleteSQL = sprintf("DELETE FROM users_projects WHERE userID='%s' AND projectID IN (%s)",
-                        users::user('ID'),
-                        implode(',', $deletedIDs));
-                    $deleteSQLResult = $engine->openDB->query($deleteSQL);
-                    if(!$deleteSQLResult['result']){
-                        throw new Exception("MySQL Error - ".$deleteSQLResult['error']);
-                    }
-                }
-
-                // Add project IDs that appeared
-                $addedIDs = array_diff($submittedProjectsIDs,$currentProjectsIDs);
-                if(sizeof($addedIDs)){
-                    $keyPairs=array();
-                    foreach($addedIDs as $addedID){
-                        $keyPairs[] = sprintf("('%s','%s')", users::user('ID'), $addedID);
-                    }
-                    $insertSQL = sprintf("INSERT INTO  users_projects (userID,projectID) VALUES %s", implode(',', $keyPairs));
-                    $insertSQLResult = $engine->openDB->query($insertSQL);
-                    if(!$insertSQLResult['result']){
-                        throw new Exception("MySQL Error - ".$insertSQLResult['error']);
-                    }
-                }
-
-                // If we get here either nothing happened, or everything worked (no errors happened)
-                $result = array(
-                    'success'    => TRUE,
-                    'deletedIDs' => $deletedIDs,
-                    'addedIDs'   => $addedIDs
-                    );
-
-            }catch(Exception $e){
-                $result = array(
-                    'success'  => FALSE,
-                    'errorMsg' => $e->getMessage()
-                    );
-            }
-            break;
+                $result = users::updateUserProjects();
+                break;
+            default:
+                break;
         }
     }
     else if (isset($engine->cleanGet['MYSQL']['action'])) {
