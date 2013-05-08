f4cf425d21f894dd1072e850022687eaf274cd4a
MichaelRBond <mbond@the-forgotten.org>
2013-05-08 11:10:52 -0400

    new method updateUserProjects(). moved from ajaxHandler

--- includes/users.php
+++ includes/users.php
@@ -130,6 +130,54 @@ class users {
 
     }
 
+    public static function updateUserProjects() {
+        $currentProjectsIDs   = array_keys(sessionGet('currentProject'));
+        $submittedProjectsIDs = isset(mfcs::$engine->cleanPost['MYSQL']['selectedProjects'])? mfcs::$engine->cleanPost['MYSQL']['selectedProjects']: array();
+
+        try{
+            // Delete project IDs that disappeared
+            $deletedIDs = array_diff($currentProjectsIDs,$submittedProjectsIDs);
+            if(sizeof($deletedIDs)){
+                $deleteSQL = sprintf("DELETE FROM users_projects WHERE userID='%s' AND projectID IN (%s)",
+                    users::user('ID'),
+                    implode(',', $deletedIDs));
+                $deleteSQLResult = mfcs::$engine->openDB->query($deleteSQL);
+                if(!$deleteSQLResult['result']){
+                    throw new Exception("MySQL Error - ".$deleteSQLResult['error']);
+                }
+            }
+
+            // Add project IDs that appeared
+            $addedIDs = array_diff($submittedProjectsIDs,$currentProjectsIDs);
+            if(sizeof($addedIDs)){
+                $keyPairs=array();
+                foreach($addedIDs as $addedID){
+                    $keyPairs[] = sprintf("('%s','%s')", users::user('ID'), $addedID);
+                }
+                $insertSQL = sprintf("INSERT INTO  users_projects (userID,projectID) VALUES %s", implode(',', $keyPairs));
+                $insertSQLResult = mfcs::$engine->openDB->query($insertSQL);
+                if(!$insertSQLResult['result']){
+                    throw new Exception("MySQL Error - ".$insertSQLResult['error']);
+                }
+            }
+
+            // If we get here either nothing happened, or everything worked (no errors happened)
+            $result = array(
+                'success'    => TRUE,
+                'deletedIDs' => $deletedIDs,
+                'addedIDs'   => $addedIDs
+                );
+
+        }catch(Exception $e){
+            $result = array(
+                'success'  => FALSE,
+                'errorMsg' => $e->getMessage()
+                );
+        }
+
+        return $result;
+    }
+
 }
 
 ?>
