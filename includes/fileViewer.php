61d05172b760f89b8d74aedf0c89790d8d56373f
David Gersting <david.gersting@mail.wvu.edu>
2013-05-14 10:15:48 -0400

    Updated fileViewer.php

--- includes/fileViewer.php
+++ includes/fileViewer.php
@@ -6,46 +6,35 @@ $engine->obCallback = FALSE;
 
 try{
 	// Check for simple (stupid developer errors)
-	if(!isset($engine->cleanGet['MYSQL']['objectID'])) throw new Exception('No Object ID Provided!');
-	if(!isset($engine->cleanGet['MYSQL']['field']))    throw new Exception('No Object Field Provided!');
+	if(!isset($engine->cleanGet['MYSQL']['assetsID'])) throw new Exception('No Assets ID Provided!');
 
-	// Get the passed info
-	$objectID  = $engine->cleanGet['MYSQL']['objectID'];
-	$fieldName = $engine->cleanGet['MYSQL']['field'];
-	$basePath  = files::getBaseUploadPath();
+	$fileType = isset($engine->cleanGet['MYSQL']['type'])
+		? $engine->cleanGet['MYSQL']['type']
+		: NULL;
+	$savePath = files::getSaveDir($engine->cleanGet['MYSQL']['assetsID'], $fileType);
 
-	// Are we getting a file from a current object, or from a revision?
-	if(isset($engine->cleanGet['MYSQL']['revisionID'])){
-		// Lookup the passed revision
-		$revisions = new revisionControlSystem('objects','revisions','ID','modifiedTime');
-		$object = $revisions->getRevision($objectID, $engine->cleanGet['MYSQL']['revisionID']);
-		if($object === FALSE) throw new Exception('Invalid Revision ID!');
-		$object['data'] = decodeFields($object['data']);
+	// Figure out what file to display (either the provided one, or the 1st one in the dir)
+	if(isset($engine->cleanGet['MYSQL']['file'])){
+		// Use provided filename
+		$filepath = $savePath.$engine->cleanGet['MYSQL']['file'];
+		errorHandle::newError("fileViewer.php - No file found at '$filepath'", errorHandle::HIGH);
+		if(!file_exists($filepath)) throw new Exception('No file found!');
 	}else{
-		// Lookup the passed object
-		$object = objects::get($objectID);
-		if($object === FALSE) throw new Exception('Invalid Object ID!');
+		// Use 1st file in directory
+		$files = scandir($savePath);
+		while(!isset($filepath) and ($file = array_pop($files)) !== FALSE){
+			if($file{0} == '.') continue;
+			$filepath = $savePath.$file;
+			if(file_exists($filepath)) break;
+		}
+		if(!isset($filepath)){
+			errorHandle::newError("fileViewer.php - No file found under '$savePath'", errorHandle::HIGH);
+			throw new Exception('No file found!');
+		}
 	}
-
-	// Get the passed field's data
-	if(isset($object['data'][$fieldName])){
-		$fieldData = $object['data'][$fieldName];
-	}else{
-		throw new Exception('Invalid Object Field!');
-	}
-
-	// Get the object's files array and grab the correct file we are showing
-	$files = $object['data'][$fieldName];
-	if(!sizeof($files)) throw new Exception("No files uploaded for this field!");
-	$file = isset($engine->cleanGet['MYSQL']['fileKey'])
-		? $files[$engine->cleanGet['MYSQL']['fileKey']]
-		: array_pop($files);
-
-	// Build the full path to the object we're showing
-	$fullPath = files::getSaveDir('originals',$file['filepath']).DIRECTORY_SEPARATOR.basename($file['filepath']);
 
 	// Get the object's contents
-	$fileContents = file_get_contents($fullPath);
+	$fileContents = file_get_contents($filepath);
 
 	// Get the MIME Type
 	if(isPHP('5.3')){
@@ -58,16 +47,16 @@ try{
 
 	// Set the correct MIME-Type headers, and output the file's content
 	if(isset($engine->cleanGet['MYSQL']['download']) and str2bool($engine->cleanGet['MYSQL']['download'])){
-		header(sprintf("Content-Disposition: attachment; filename='%s'", $file['filename']));
+		header(sprintf("Content-Disposition: attachment; filename='%s'", basename($filepath)));
 		header("Content-Type: application/octet-stream");
 		die($fileContents); // die so nothing else will be displayed
 	}else{
 		if($mimeType == 'application/x-empty'){
-			errorHandle::newError("Failed to locate file to display! (objectID: {$engine->cleanGet['MYSQL']['objectID']}, field: {$engine->cleanGet['MYSQL']['field']})", errorHandle::HIGH);
+			errorHandle::newError("Failed to locate file to display!", errorHandle::HIGH);
 			header("Content-type: text/plain");
 			die("Failed to locate requested file!"); // die so nothing else will be displayed
 		}else{
-			files::generateFilePreview($fullPath, $mimeType, $fileContents);
+			files::generateFilePreview($filepath, $mimeType, $fileContents);
 			exit();
 		}
 	}
