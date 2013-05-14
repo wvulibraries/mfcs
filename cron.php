4cf1124f0ebaa5e6f79e52f82af643e8be64dc48
David Gersting <david.gersting@mail.wvu.edu>
2013-05-14 15:32:15 -0400

    Created cron file and started with uploads cleaning

---
+++ cron.php
@@ -0,0 +1,18 @@
+#!/usr/bin/php
+<?php
+$config = parse_ini_file(__DIR__.DIRECTORY_SEPARATOR."config.ini");
+
+# Delete old, abandoned, upload directories
+####################################################################
+$uploadPath = $config['uploadPath'];
+$uploadDirs = scandir($uploadPath);
+foreach($uploadDirs as $uploadDir){
+	// Skip hidden stuff
+	if($uploadDir{0} == '.') continue;
+
+	$uploadDir = $uploadPath.DIRECTORY_SEPARATOR.$uploadDir;
+	$mTime     = filemtime($uploadDir);
+	if(($mTime+$config['uploadPath']) <= time()){
+		shell_exec("sudo rm -rf $uploadDir");
+	}
+}
