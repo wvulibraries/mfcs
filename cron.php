5ec615b656f615540d6e00573f84e182993cf4e4
MichaelRBond <mbond@the-forgotten.org>
2013-05-15 07:13:41 -0400

    switched to join for directory path (more maintainable), commented out deletes and added todos/comments

--- cron.php
+++ cron.php
@@ -10,10 +10,14 @@ foreach($uploadDirs as $uploadDir){
 	// Skip hidden stuff
 	if($uploadDir[0] == '.') continue;
 
-	$uploadDir = $uploadPath.DIRECTORY_SEPARATOR.$uploadDir;
+	$uploadDir = implode(DIRECTORY_SEPARATOR,array($uploadPath,$uploadDir);
 	$mTime     = filemtime($uploadDir);
 	if(($mTime+$config['uploadPath']) <= time()){
-		shell_exec("sudo rm -rf $uploadDir");
+		// @TODO apache cannot use 'sudo' for security reasons
+		// @TODO we do not use rm -rf in an automated script, o the chance that something goes wrong. 
+		//  These files will need to be unlinked using php functions
+		//  There should be a sanity check to make sure we are not deleting starting at / or in the archives directory by accident. 
+		// shell_exec("sudo rm -rf $uploadDir");
 	}
 }
 ?>
