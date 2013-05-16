60dfeb0ef6b08cb5ffab7f58ba78575cc4e2a3c9
MichaelRBond <mbond@the-forgotten.org>
2013-05-16 10:22:02 -0400

    removed tables that are no longer in the system

--- includes/testing/truncate.php
+++ includes/testing/truncate.php
@@ -4,12 +4,10 @@ include("../../header.php");
 
 
 $sql       = array();
-$sql[] = "TRUNCATE TABLE `containers`";
 $sql[] = "TRUNCATE TABLE `dupeMatching`";
 $sql[] = "TRUNCATE TABLE `forms`";
 $sql[] = "TRUNCATE TABLE `objectMetadataLinks`";
 $sql[] = "TRUNCATE TABLE `objectProjects`";
-$sql[] = "TRUNCATE TABLE `objectTypes`";
 $sql[] = "TRUNCATE TABLE `objects`";
 $sql[] = "TRUNCATE TABLE `permissions`";
 $sql[] = "TRUNCATE TABLE `projects`";
