fdd3e9d9a7f0e8fe2ac9738d7350c061dbdccfb6
Michael Bond <mbond@th-forgotten.org>
2013-10-10 09:25:38 -0400

    removed CSS from revisions.php and moved to external file

---
+++ dataEntry/headerIncludes.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" type="text/css" href="{local var="siteRoot"}includes/css/revisions.css">
--- dataEntry/revisions.php
+++ dataEntry/revisions.php
@@ -113,53 +113,6 @@ $engine->eTemplate("include","header");
 	</section>
 </div>
 
-<!-- @TODO : Style should be moved out of this file -->
-<style>
-	.revisionSection{
-		width: 45%;
-		margin: 5px;
-		display: inline-block;
-		vertical-align: top;
-	}
-	.revisionSection header{
-		font-size: 20px;
-		font-weight: bold;
-		border-bottom: 1px solid #000;
-		padding: 5px;
-	}
-	.revisionSection header div{
-		float: right;
-		font-weight: normal;
-		font-size: 15px;
-	}
-	.revisionSection header div select{
-		padding: 0;
-		margin: 0;
-		height: inherit;
-	}
-
-	.objectField{
-		margin: 5px 15px;
-	}
-	.objectField header{
-		font-weight: bold;
-		font-size: 14px;
-		padding: 0;
-		border-bottom: 1px solid #999;
-		width: 50%;
-	}
-	.objectField ul{
-		margin-left: 10px;
-	}
-	.objectField aside{
-		border-top: 1px solid #ccc;
-		width: 25%;
-		margin-top: 10px;
-		padding: 5px 0;
-	}
-</style>
-
-
 <script src="{local var="siteRoot"}includes/js/revisions.js" type="text/javascript" id="revisionsScript" data-objectid="{local var="objectID"}"></script>
 
 <?php
