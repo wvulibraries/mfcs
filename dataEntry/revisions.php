ca08e5a76ea2f649595b54588a6b9b4611c9a4fa
Michael Bond <mbond@th-forgotten.org>
2013-10-10 09:18:50 -0400

    moved javascript from revisions.php to a separate file.

--- dataEntry/revisions.php
+++ dataEntry/revisions.php
@@ -159,52 +159,8 @@ $engine->eTemplate("include","header");
 	}
 </style>
 
-<!-- @TODO : scripts should be moved out of this file -->
-<script>
-	function scrollSync(iFrameObj){
-		$($(iFrameObj).contents()).scroll(function(){
-			if($('#revisionSelector').val()){
-				var thisObj     = $(this);
-				var url         = thisObj[0].URL;
-				var fieldName   = url.match(/field=\w+/i)[0].split('=')[1];
-				var iFrameClass = url.match(/#\w+/i)[0].substr(1);
-				var targetClass = iFrameClass=='rightFileViewer' ? 'leftFileViewer' : 'rightFileViewer';
-				var scrollTop   = thisObj.scrollTop();
-				var scrollLeft  = thisObj.scrollLeft();
-				$('.'+targetClass+'[data-field_name="'+fieldName+'"]').contents().scrollTop(scrollTop).scrollLeft(scrollLeft);
-			}
-		});
-		// onLoad, trigger sync from the left fileViewer
-		$('.leftFileViewer').contents().scroll();
-	}
 
-	$(function(){
-		$('#revisionSelector').change(function(){
-			var url = '?objectID={local var="objectID"}&revisionID='+$(this).val();
-			$('#revisionViewer').load(url);
-		});
-		$('#revertBtn').click(function(){
-			if(confirm('Are you sure you want to revert back to this version?')){
-				$('#revisionID').val( $('#revisionSelector').val() );
-				$('#revisionForm').submit();
-				$('#revisions :input').attr('disabled','disabled');
-			}else{
-				alert('Revert canceled');
-			}
-		});
-		$('#objectComparator').on('click','.toggleFileList',function(){
-			$link = $(this);
-			$ul   = $link.next('ul');
-			if($ul.is(':visible')){
-				$ul.slideUp('fast');
-				$link.html('click to show list');
-			}else{
-				$ul.slideDown('fast');
-				$link.html('click to hide list');
-			}
-		});
-	});
-</script>
+<script src="{local var="siteRoot"}includes/js/revisions.js" type="text/javascript" id="revisionsScript" data-objectid="{local var="objectID"}"></script>
 
 <?php
 $engine->eTemplate("include","footer");
