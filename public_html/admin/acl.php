670244562f959d8e55072bb9facc248d6232b53c
Michael Bond <mbond@th-forgotten.org>
2013-11-07 10:37:54 -0500

    added checks to see if we are running on localhost. if yes, don't check the acts (for vagrant development)

--- public_html/admin/acl.php
+++ public_html/admin/acl.php
@@ -1,6 +1,9 @@
 <?php
-$engine->accessControl("ADgroup","libraryGroup_staff",TRUE,FALSE);
-$engine->accessControl("ADgroup","libraryGroup_students",TRUE,FALSE);
-$engine->accessControl("denyAll");
-$engine->accessControl("build");
+// prevent ACLs on vagrant
+if ($_SERVER['SERVER_NAME'] != "localhost") { 
+	$engine->accessControl("ADgroup","libraryGroup_staff",TRUE,FALSE);
+	$engine->accessControl("ADgroup","libraryGroup_students",TRUE,FALSE);
+	$engine->accessControl("denyAll");
+	$engine->accessControl("build");
+}
 ?>
