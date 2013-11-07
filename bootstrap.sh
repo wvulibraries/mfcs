a6a62acb6d69fe77b99011bdf9b99026d724cdbe
Michael Bond <mbond@th-forgotten.org>
2013-11-07 08:33:22 -0500

    additional fixes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Base Setup
+# Base PRE Setup
 
 GITDIR="/tmp/git"
 ENGINEAPIGIT="https://github.com/wvulibraries/engineAPI.git"
@@ -12,7 +12,7 @@ DOCUMENTROOT="public_html"
 yum -y install httpd httpd-devel httpd-manual httpd-tools
 yum -y install mysql-connector-java mysql-connector-odbc mysql-devel mysql-lib mysql-server
 yum -y install mod_auth_kerb mod_auth_mysql mod_authz_ldap mod_evasive mod_perl mod_security mod_ssl mod_wsgi 
-yum -y install phpkk php-bcmath php-cli php-common php-gd php-ldap php-mbstring php-mcrypt php-mysql php-odbc php-pdo php-pear php-pear-Benchmark php-pecl-apc php-pecl-imagick php-pecl-memcache php-soap php-xml php-xmlrpc 
+yum -y install php php-bcmath php-cli php-common php-gd php-ldap php-mbstring php-mcrypt php-mysql php-odbc php-pdo php-pear php-pear-Benchmark php-pecl-apc php-pecl-imagick php-pecl-memcache php-soap php-xml php-xmlrpc 
 yum -y install emacs emacs-common emacs-nox
 yum -y install git
 
@@ -28,10 +28,17 @@ ln -s $GITDIR/engineAPI/engine/ $SERVERURL/phpincludes/
 
 # Application Specific
 
-rmdir $SERVERURL/$DOCUMENTROOT
 ln -s /vagrant/public_html $SERVERURL/$DOCUMENTROOT
 ln -s $SERVERURL/phpincludes/engine/engineAPI/3.1 $SERVERURL/phpincludes/engine/engineAPI/latest
 
-cp /vagrant/serverConfiguration/php.ini /etc/php.ini
-/etc/init.d/httpd
+rm -f /etc/php.ini
+rm -f /etc/httpd/conf/httpd.conf
 
+ln -s /vagrant/serverConfiguration/php.ini /etc/php.ini
+ln -s /vagrant/serverConfiguration/vagrant_httpd.conf /etc/httpd/conf/httpd.conf
+
+/etc/init.d/httpd restart
+
+# Base Post Setup
+
+ln -s /tmp/git/engineAPI/public_html/engineIncludes $SERVERURL/$DOCUMENTROOT/engineIncludes
