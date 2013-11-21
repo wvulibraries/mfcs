cea0bb7f37b34e6e4e4e1bbf499fba6b8a570c64
Michael Bond <mbond@th-forgotten.org>
2013-11-21 14:33:03 -0500

    initial 3rd party installer. Doesn't Work Yet.

--- bootstrap.sh
+++ bootstrap.sh
@@ -75,3 +75,40 @@ do
   echo "Processing $f ..."
   mysql -u root mfcs < $f
 done
+
+#install 3rd Party dependencies
+# cd /vagrant/serverConfiguration/3rdParty
+# rpm -Uvh --force --quiet remi-release-6*.rpm epel-release-6*.rpm
+
+# yum -y install ImageMagick php-pecl-imagick 
+
+# rm -f /etc/yum.repos.d/remi.repo
+# ln -s /vagrant/serverConfiguration/remi.repo /etc/yum.repos.d/remi.repo
+
+# yum -y install libjpeg-devel libpng-devel libtiff-devel SDL-devel agg-devel
+
+# tar -zxf /vagrant/serverConfiguration/3rdParty/leptonica-1.69.tar.gz --directory=/tmp
+# tar -zxf /vagrant/serverConfiguration/3rdParty/tesseract-ocr-3.02.02.tar.gz --directory=/tmp
+# tar -zxf /vagrant/serverConfiguration/3rdParty/tesseract-ocr-3.02.eng.tar.gz --directory=/tmp
+# tar -xf /vagrant/serverConfiguration/3rdParty/exact-image-0.8.8.tar --directory=/tmp
+
+# cd /tmp/leptonica-1.69
+# ./configure
+# make
+# make install
+
+# cd /tmp/tesseract-ocr
+# ./autogen.sh
+# ./configure
+# make
+# make install
+
+# cp /tmp/tesseract-ocr/tessdata/eng.* /usr/local/share/tessdata
+
+# cd /tmp/exact-image-0.8.8
+# ./configure
+# make
+# make install
+
+# ln -s /usr/local/bin/tesseract /usr/bin/
+# ln -s /usr/local/bin/hocr2pdf /usr/bin/
