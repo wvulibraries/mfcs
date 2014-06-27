8d8dc0321ae2fc2ba6552a6b0753af6fae528f3e
Scott Blake <Scott.Blake@mail.wvu.edu>
2014-06-27 08:37:02 -0400

    Add 'drop database' and 'use mfcs' SQL

--- SQLFiles/setup.sql
+++ SQLFiles/setup.sql
@@ -1,3 +1,4 @@
-CREATE DATABASE mfcs;
-
-GRANT ALL PRIVILEGES ON mfcs.* TO 'username'@'localhost';
+DROP DATABASE IF EXISTS `mfcs`;
+CREATE DATABASE IF NOT EXISTS `mfcs`;
+GRANT ALL PRIVILEGES ON `mfcs`.* TO 'username'@'localhost';
+USE `mfcs`;
