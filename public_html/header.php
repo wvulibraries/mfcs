65338b45c8a3a1b4a07f2b188a0145a4567cfdeb
Michael Bond <mbond@the-forgotten.org>
2015-09-08 21:00:32 -0400

    aborts MFCS if the unique idno check fails.
    This is ignored on the CLI.

--- public_html/header.php
+++ public_html/header.php
@@ -23,6 +23,35 @@ require_once "includes/revisions.php";
 require_once "includes/exporting.php";
 mfcs::singleton();
 
+// Quick and dirty Checks check
+// @TODO this needs to be more formalized in a class to easily include other checks as well
+if (!isCLI()) {
+	$sql_check       = sprintf("SELECT `value` FROM `checks` WHERE `name`='uniqueIDCheck'");
+	$sqlResult_check = mfcs::$engine->openDB->query($sql_check);
+
+	if (!$sqlResult_check['result']) {
+		errorHandle::newError(__METHOD__."() - : ".$sqlResult['error'], errorHandle::DEBUG);
+
+		print "<p>Error checking MFCS sanity. Aborting.</p>";
+
+		exit;
+	}
+
+	$row_check = mysql_fetch_array($sqlResult_check['result'],  MYSQL_ASSOC);
+
+	if ($row_check['value'] == "0") {
+
+	// notify systems via email
+		print "<h1>ERROR!</h1>";
+		print "<p>MFCS Failed idno sanity check. Please contact systems Immediately.</p>";
+		print "<p>Please jot down the steps you took getting to this point. Be as specific as possible.</p>";
+		print "<p>Aborting.</p>";
+		exit;
+	}
+}
+
+// End Checks
+
 $mfcsSearch = new mfcsSearch();
 
 // Load the user's current projects
