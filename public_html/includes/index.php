0a7b8aa53c96e9efde72fbdeceae61f1821b1388
Michael Bond <mbond@the-forgotten.org>
2015-10-01 08:42:10 -0400

    List the includes alphabetically.

--- public_html/includes/index.php
+++ public_html/includes/index.php
@@ -1,19 +1,20 @@
 <?php 
 
 require_once "mfcs.php";
+
+require_once "duplicates.php";
+require_once "exporting.php";
+require_once "files.php";
 require_once "forms.php";
 require_once "list.php";
+require_once "log.php";
 require_once "navigation.php";
 require_once "objects.php";
 require_once "permissions.php";
 require_once "projects.php";
+require_once "revisions.php";
 require_once "search.php";
 require_once "stats.php"; 
 require_once "users.php";
-require_once "files.php";
-require_once "duplicates.php";
-require_once "revisions.php";
-require_once "exporting.php";
-require_once "log.php";
 
 ?>
