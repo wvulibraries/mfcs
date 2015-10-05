8ad35a580aa72883f6008c451b35f5a324698ecf
Michael Bond <mbond@the-forgotten.org>
2015-10-05 09:08:14 -0400

    Add date tot he logging method

--- public_html/includes/classes/log.php
+++ public_html/includes/classes/log.php
@@ -4,13 +4,14 @@ class log {
 
 	public static function insert($action,$objectID=0,$formID=0,$info=NULL) {
 
-		$sql       = sprintf("INSERT INTO `logs` (`username`,`IP`,`action`,`objectID`,`formID`,`info`) VALUES('%s','%s','%s','%s','%s','%s')",
+		$sql       = sprintf("INSERT INTO `logs` (`username`,`IP`,`action`,`objectID`,`formID`,`info`,`date`) VALUES('%s','%s','%s','%s','%s','%s','%s')",
 			mfcs::$engine->openDB->escape(users::user('username')),
 			mfcs::$engine->openDB->escape($_SERVER['REMOTE_ADDR']),
 			mfcs::$engine->openDB->escape($action),
 			mfcs::$engine->openDB->escape($objectID),
 			mfcs::$engine->openDB->escape($formID),
-			mfcs::$engine->openDB->escape($info)
+			mfcs::$engine->openDB->escape($info),
+			time()
 			);
 		$sqlResult = mfcs::$engine->openDB->query($sql);
 		
