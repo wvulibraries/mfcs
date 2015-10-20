af7a216c90d982cbc664f49ddaf44cf638db43b4
ddavisgraphics <ddavisgraphics@gmail.com>
2015-10-20 15:23:38 -0400

    Application Header for Cleaned Template

---
+++ public_html/includes/appHeader.php
@@ -0,0 +1,9 @@
+<header class="applicationHeader">
+    <div class="mfcs-logo">
+        <img src="/images/mfcs.png" alt="Metadata Form Creation System" />
+    </div>
+    <h1>
+        <span class="desktop"> Metadata Form Creation System </span>
+        <span class="mobile"> MFCS </span>
+    </h1>
+</header>
