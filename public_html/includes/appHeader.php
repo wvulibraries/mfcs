d1cf054123296c9babd956f5959f2ec902965266
ddavisgraphics <ddavisgraphics@gmail.com>
2015-10-21 10:04:40 -0400

    application headers and modals

--- public_html/includes/appHeader.php
+++ public_html/includes/appHeader.php
@@ -2,8 +2,42 @@
     <div class="mfcs-logo">
         <img src="/images/mfcs.png" alt="Metadata Form Creation System" />
     </div>
-    <h1>
-        <span class="desktop"> Metadata Form Creation System </span>
-        <span class="mobile"> MFCS </span>
-    </h1>
+
+    <div class="site-title">
+        <h1>
+            <span class="desktop"> Metadata Form Creation System </span>
+            <span class="mobile"> MFCS </span>
+        </h1>
+    </div>
+
+    <div class="projectsToggle">
+        <a href="javascript:void(0)" class="projectToggle">
+            <i class="fa fa-folder-open"></i> Select Projects
+        </a>
+    </div>
 </header>
+
+<!-- Modal - Select Current Projects -->
+<div id="selectProjectsModal" class="modal hide" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-header">
+        <button type="button" class="close" aria-hidden="true">×</button>
+        <h3>Your current projects:</h3>
+    </div>
+    <div class="modal-body">{local var="projectModalList"}</div>
+    <div class="modal-footer">
+        <button class="btn cancel" aria-hidden="true">Cancel</button>
+        <button class="btn btn-primary submitProjects">Save changes</button>
+    </div>
+</div>
+
+<div id="metadataModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="metadataEntryFormModal" aria-hidden="true">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h3 id="metadataModalHeader"></h3>
+    </div>
+    <div class="modal-body" id="metadataModalBody"></div>
+    <div class="modal-footer">
+        <button class="btn"  class="close" aria-hidden="true">Cancel</button>
+        <button class="btn btn-primary" onclick="submitMetadataModal();">Save changes</button>
+    </div>
+</div>
