ae63ca99b7f5a2a991128ed5ba68bc3aea69c254
ddavisgraphics <ddavisgraphics@gmail.com>
2015-10-26 10:26:21 -0400

    Changed places for Modals

--- public_html/footerIncludes.php
+++ public_html/footerIncludes.php
@@ -23,27 +23,3 @@ $projectListHTML .= '</ul>';
 localvars::add('projectModalList', $projectListHTML);
 ?>
 {engine name="csrf"}
-<!-- Modal - Select Current Projects -->
-<div id="selectProjectsModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-		<h3>Your current projects:</h3>
-	</div>
-	<div class="modal-body">{local var="projectModalList"}</div>
-	<div class="modal-footer">
-		<button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
-		<button class="btn btn-primary" onclick="saveSelectedProjects();">Save changes</button>
-	</div>
-</div>
-
-<div id="metadataModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="metadataEntryFormModal" aria-hidden="true">
-	<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-		<h3 id="metadataModalHeader"></h3>
-	</div>
-	<div class="modal-body" id="metadataModalBody"></div>
-	<div class="modal-footer">
-		<button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
-		<button class="btn btn-primary" onclick="submitMetadataModal();">Save changes</button>
-	</div>
-</div>
