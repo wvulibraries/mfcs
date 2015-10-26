975c027b6f5e7b5dd86be8c81fdcc1b374c04c72
ddavisgraphics <ddavisgraphics@gmail.com>
2015-10-26 10:26:11 -0400

    Added JS to Libs Min JS and moved styles to SCSS

--- public_html/headerIncludes.php
+++ public_html/headerIncludes.php
@@ -18,32 +18,6 @@ $fineuploader = array(
 	$path."dataEntry/object.php",
 	$path."dataView/object.php",
 	);
-
-if (in_array($_SERVER['SCRIPT_NAME'], $fineuploader)) {
-	?>
-	<link rel="stylesheet" type="text/css" href="{local var="siteRoot"}includes/css/fineuploader.css" />
-	<script type="text/javascript" src="{local var="siteRoot"}includes/js/jquery.fineuploader.min.js"></script>
-	<style>
-		/* Fine Uploader
-		-------------------------------------------------- */
-		.qq-upload-list {
-			text-align: left;
-		}
-
-		li.alert-success {
-			background-color: #DFF0D8;
-		}
-
-		li.alert-error {
-			background-color: #F2DEDE;
-		}
-
-		.alert-error .qq-upload-failed-text {
-			display: inline;
-		}
-	</style>
-	<?php
-}
 ?>
 
 <script>
