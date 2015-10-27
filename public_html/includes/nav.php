a75f29e9878611082ea771e94e1d5c3db48788e6
ddavisgraphics <ddavisgraphics@gmail.com>
2015-10-27 15:46:37 -0400

    creating navigation

---
+++ public_html/includes/nav.php
@@ -0,0 +1,39 @@
+<nav class="main-nav">
+    <h2> Menu </h2>
+    <div class="close"> <a href="javascript:void(0);"> <i class="fa fa-times-circle"></i> </a> </div>
+    <ul>
+        <div>
+            <li><a href="/"> <i class="fa fa-home"></i> Home</a></li>
+        </div>
+
+        <div>
+            <li> <h3> Data Entry </h3> </li>
+            <li><a href="/dataEntry/selectForm.php"><i class="fa fa-angle-double-right"></i> Create</a></li>
+            <li><a href="/dataView/list.php"><i class="fa fa-angle-double-right"></i> List</a></li>
+            <li><a href="/dataView/search.php"><i class="fa fa-angle-double-right"></i> Search</a></li>
+        </div>
+
+        <div>
+            <li> <h3> Form Management </h3> </li>
+            <li><a href="/formCreator/"> <i class="fa fa-angle-double-right"></i> New Form</a></li>
+            <li><a href="/formCreator/list.php"> <i class="fa fa-angle-double-right"></i> List Forms</a></li>
+            <li><a href="/formCreator/copy.php"> <i class="fa fa-angle-double-right"></i> Copy Form</a></li>
+        </div>
+
+        <div>
+            <li> <h3> Administration </h3> </li>
+            <!-- <li><a href="/admin/fileReProcessing/">File Re-Processing</a></li> -->
+            <li><a href="/admin/projects/"><i class="fa fa-angle-double-right"></i> Projects</a></li>
+            <li><a href="/admin/users/"><i class="fa fa-angle-double-right"></i> Users</a></li>
+            <li><a href="/admin/watermarks/"> <i class="fa fa-angle-double-right"></i> Watermarks</a></li>
+        </div>
+
+         <div>
+            <li> <h3> Logout </h3> </li>
+            <li><a href="https://mfcsdev.lib.wvu.edu/engineIncludes/logout.php?csrf=d648314d45bbce117f76bdcdb0810aa0"> <i class="fa fa-user-times"></i> Logout</a></li>
+        </div>
+    </ul>
+</nav>
+
+
+
