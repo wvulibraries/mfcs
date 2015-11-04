feefb82dfa7f978c1434c677348b89296e97f190
ddavisgraphics <ddavisgraphics@gmail.com>
2015-11-04 15:45:06 -0500

    no need for this file now.

--- public_html/formCreator/footerIncludes.php
+++
@@ -1,3 +0,0 @@
-<!-- <script type="text/javascript" src='{local var="siteRoot"}includes/js/createForm_nav.js'></script>
-<script type="text/javascript" src='{local var="siteRoot"}includes/js/createForm_form.js'></script>
-<script type="text/javascript" src='{local var="siteRoot"}includes/js/createForm_permissions.js'></script> -->
