f5e23f48aefcca924a490f75f5b730b3dc16a132
Michael Bond <mbond@the-forgotten.org>
2015-11-09 07:29:26 -0500

    Move locking logic to lock class

--- public_html/includes/classes/objects.php
+++ public_html/includes/classes/objects.php
@@ -859,22 +859,7 @@ class objects {
 	 */
 	public static function is_locked($objectID) {
 
-		$sql       = sprintf("SELECT COUNT(*) from `locks` WHERE `type`='object' AND `typeID`='%s'",
-			mfcs::$engine->openDB->escape($objectID));
-		$sqlResult = $engine->openDB->query($sql);
-		
-		if (!$sqlResult['result']) {
-			errorHandle::newError(__METHOD__."() - : ".$sqlResult['error'], errorHandle::DEBUG);
-			return FALSE;
-		}
-		
-		$row = mysql_fetch_array($sqlResult['result'],  MYSQL_ASSOC);
-
-		if ($row["COUNT(*)"] != 0) {
-			return TRUE;
-		}
-
-		return FALSE;
+		return lock::is_locked($obejctID,"object");
 
 	}
 
@@ -885,24 +870,7 @@ class objects {
 	 */
 	public static function lock($objectID) {
 
-		if (self::is_locked($objectID)) {
-			errorHandle::errorMsg("Object already locked.");
-			return FALSE;
-		}
-
-		$sql       = sprintf("INSERT INTO `locks` (`type`,`typeID`,`user`,`date`) VALUES('object','%s','%s','%s')",
-			mfcs::$engine->openDB->escape($objectID),
-			mfcs::$engine->openDB->escape(users::user('ID')),
-			time()
-			);
-		$sqlResult = $engine->openDB->query($sql);
-		
-		if (!$sqlResult['result']) {
-			errorHandle::newError(__METHOD__."() - : ".$sqlResult['error'], errorHandle::DEBUG);
-			return FALSE;
-		}
-		
-		return $sqlResult['id']
+		return lock::lock($objectID,"object");
 
 	}
 
@@ -913,16 +881,7 @@ class objects {
 	 */
 	public static function unlock($objectID) {
 
-		$sql       = sprintf("DELETE FROM `locks` WHERE `type`='object' AND `typeID`='%s'",
-			mfcs::$engine->openDB->escape($objectID));
-		$sqlResult = $engine->openDB->query($sql);
-		
-		if (!$sqlResult['result']) {
-			errorHandle::newError(__METHOD__."() - unlocking object: ".$sqlResult['error'], errorHandle::DEBUG);
-			return FALSE;
-		}
-		
-		return TRUE;
+		return lock::unlock($objectID,"object");
 
 	}
 
