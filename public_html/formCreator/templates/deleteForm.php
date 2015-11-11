f52c90d4d9ebf41b6a092f2b43dd69dbee38ae4b
ddavisgraphics <ddavisgraphics@gmail.com>
2015-11-11 10:13:31 -0500

    Modified view

--- public_html/formCreator/templates/deleteForm.php
+++ public_html/formCreator/templates/deleteForm.php
@@ -5,11 +5,14 @@
     <li><a href="{local var="siteRoot"}formCreator/">Form Creator</a></li>
     <li> Delete Form </li>
 </ul>
+
 <form method="post" action="" id="deleteFormFrm">
     {engine name="csrf"}
     <input type="hidden" name="deleteForm" value="deleteForm">
-    <p>Are you sure you want to delete this form?</p>
-    <p>This will permanently delete this form and all associated objects, and cannot be undone.</p>
+    <br>
+    <div class="alert alert-warning">
+        <h1 class="danger"> WARNING </h1>
+        Are you sure you want to delete this form? <br> This will permanently delete this form and all associated objects, and cannot be undone.</div>
     <input type="button" value="Cancel" class="btn" id="deleteFormBtn-Cancel">
     <input type="button" value="Delete Form" class="btn btn-danger" id="deleteFormBtn-Submit">
 </form>
