f3de0706c7e154f205743862bf44c0f1ed587fbd
Michael Bond <mbond@the-forgotten.org>
2015-11-12 09:43:31 -0500

    return the LockID when checking if something is locked
    Also, DocBlock.

--- public_html/includes/classes/locks.php
+++ public_html/includes/classes/locks.php
@@ -45,9 +45,15 @@ class locks {
 
 	}
 
+	/**
+	 * Determines if the given object is locked for editing
+	 * @param  int  $objectID the MySQL database ID number for the object
+	 * @param  string  $type     the type of "thing" (object or form) that the ID number corresponds to. 
+	 * @return boolean|int           If not locked, boolean false is returned. If locked, lock ID is returned
+	 */
 	public static function is_locked($objectID, $type) {
 
-		$sql       = sprintf("SELECT COUNT(*) from `locks` WHERE `type`='%s' AND `typeID`='%s'",
+		$sql       = sprintf("SELECT COUNT(*), `locks`.ID as ID, `users`.`username` as `username`,`date` from `locks` LEFT JOIN users on `locks`.`user`=`users`.`ID` WHERE `type`='%s' AND `typeID`='%s'",
 			mfcs::$engine->openDB->escape($type),
 			mfcs::$engine->openDB->escape($objectID)
 			);
@@ -60,12 +66,11 @@ class locks {
 		
 		$row = mysql_fetch_array($sqlResult['result'],  MYSQL_ASSOC);
 
-		if ($row["COUNT(*)"] != 0) {
-			return TRUE;
+		if ((int)$row["COUNT(*)"] != 0) {
 			localvars::add("lockUsername",$row['username']);
 			localvars::add("lockDate",date("D,  M d Y H:i",$row['date']));
+			return $row['ID'];
 		}
-
 		return FALSE;
 
 	}
