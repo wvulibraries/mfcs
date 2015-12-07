dd32e36e6b92e7423e2aa985941b6d10dd874735
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-07 13:06:50 -0500

    Hoem page mods

--- public_html/index.php
+++ public_html/index.php
@@ -26,7 +26,7 @@ $engine->eTemplate("include","header");
 
 <section>
 	<header class="page-header">
-		<h1 class="page-title">Select a Task</h1>
+		<h1 class="page-title">Welcome to MFCS</h1>
 	</header>
 
 	<ul class="breadcrumbs">
@@ -35,49 +35,52 @@ $engine->eTemplate("include","header");
 	</ul>
 </section>
 
-<section class="createIndex">
-	<div class="widthContainer">
-		<span class="span4">
-			<div class="hero"><img src="images/mfcsLines-01.svg"></div>
-			<div class="text">
-				<h2> Create Objects & Forms </h2>
-				<p>Creating forms allow you to collect certain information along with your digital objects and is the basis for creating objects. Creating Objects in MFCS is the way your archive data for use in a digital repository later. </p>
-				<div class="actionButton">
-					<a href="" class="btn btn-primary"> Create A Form </a> <a href="" class="btn btn-primary"> Create An Object </a>
-				</div>
-			</div>
-		</span>
-		<span class="span4">
-			<div class="hero"><img src="images/mfcsLine-02.svg"></div>
-			<div class="text">
-				<h2> Manage Inventory </h2>
-				<p> MFCS allows you to manage forms, and forms that you have created.  Forms can be edited by using the list forms link in the Form Management section.  Editing objects works in a similar fashion using the Data Entry section.    </p>
-				<div class="actionButton">
-					<a href="" class="btn btn-primary"> List Forms </a> <a href="" class="btn btn-primary"> List Objects </a> <a href="" class="btn btn-primary"> Search </a>
-				</div>
-			</div>
-		</span>
-		<span class="span4">
-			<div class="hero"><img src="images/mfcsLines-03.svg"></div>
-			<div class="text">
-				<h2> System Administration </h2>
-				<p> There are other things that can be managed by system administrators.  Adding users, granting permissions, and exporting for digital repositories.  If you are need one of these tasks done contact your system administrator. </p>
-			</div>
-		</span>
-	</div>
-</section>
-
 <section class="aboutMFCS">
 	<div class="widthContainer">
 		<div class="pic">
 			<img src="images/mfcsData.png" alt="mfcs holding a data icon" />
 		</div>
 		<div class="text">
-			<h2> Welcome to Metadata Form Creation System </h2>
-			<p> MFCS is a system built to store and archive digital projects, finding aids, and historical material entrusted to the library.  This system allows us to keep secure data, achive the original, and make modifications for other technologies to use.  The matieral ultimately ends up in a repository system such as Hydra, Islandora, or DLXS. </p>
+			<p> MFCS is a system built to store and archive digital projects, finding aids, and historical material entrusted to the library.  This system allows us to keep secure data, achive the original, and make modifications for other technologies to use.  The material ultimately ends up in a repository system such as Hydra, Islandora, or DLXS. </p>
 		</div>
 	</div>
 </section>
+
+<section class="createIndex">
+	<div class="widthContainer">
+		<span class="span4">
+			<h2> <i class="fa fa-tasks"></i> Manage Inventory </h2>
+			<div class="text">
+				<p> MFCS allows you to manage forms, and forms that you have created.  Forms can be edited by using the list forms link in the Form Management section.  Editing objects works in a similar fashion using the Data Entry section.    </p> <br>
+				<div class="actionButton">
+					<a href="/dataEntry/selectForm.php" class="btn btn-primary"> Create Objects </a>
+					<a href="/dataView/list.php" class="btn btn-primary"> List Objects </a>
+					<a href="/dataView/search.php" class="btn btn-primary"> Search </a>
+				</div>
+			</div>
+		</span>
+		<span class="span4">
+			<h2> <i class="fa fa-list-alt"></i> Manage Forms </h2>
+			<div class="text">
+				<p>Creating forms allow you to collect certain information along with your digital objects and is the basis for creating objects. Creating Objects in MFCS is the way your archive data for use in a digital repository later. </p><br>
+				<div class="actionButton">
+					<a href="/formCreator/" class="btn btn-primary"> Create A Form </a>
+					<a href="/formCreator/list.php" class="btn btn-primary"> List Forms </a>
+				</div>
+			</div>
+		</span>
+		<span class="span4">
+			<h2> <i class="fa fa-book"></i> Documentation </h2>
+			<div class="text">
+				<p> Need help?  The documentation will help to provide some of the basic jargon and techniques to accomplishing tasks in MFCS.  </p><br>
+				<div class="actionButton">
+					<a href="https://github.com/wvulibraries/mfcs/wiki" class="btn btn-primary"> MFCS Wiki </a>
+				</div>
+			</div>
+		</span>
+	</div>
+</section>
+
 
 <section>
 	<ul>
