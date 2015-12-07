e8794042728ac3c8c1d73ad9d6a7040b144b5feb
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-07 09:49:50 -0500

    Template Added to MFCS and Boostrap Updated

---
+++ template/mfcsTemplate/templateFooter.php
@@ -0,0 +1,8 @@
+<?php
+    print file_get_contents("https://lib.wvu.edu/index.php?location=/templateIncludes/2014/templateFooter.php&releaseType=1");
+?>
+
+<?php recurseInsert("footerIncludes.php","php") ?>
+
+</body>
+</html>
---
+++ template/mfcsTemplate/templateHeader.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>{local var="currentDisplayObjectTitle"} | WVU Libraries | West Virginia University</title>
+
+    <!-- Meta Information -->
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="HandheldFriendly" content="True">
+
+    <!-- Author, Description, Favicon, and Keywords -->
+        <meta name="author" content="WVU Libraries | {local var="meta_authors"}">
+        <meta name="description" content="{local var="meta_description"}">
+        <meta name="keywords" content="{local var="meta_keywords"}">
+        <link rel="shortcut icon" href="https://wvrhc.lib.wvu.edu/favicon.ico">
+
+	 <!-- Project Specific Head Includes -->
+		<?php recurseInsert("headerIncludes.php","php") ?>
+</head>
+
+<body>
+
+ <!-- WebApp Header -->
+    <?php recurseInsert("includes/appHeader.php","php") ?>
+
+ <!-- Navigation -->
+    <?php recurseInsert("includes/nav.php","php") ?>
+
