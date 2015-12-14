ec2474fd4113ce524f8abf208c850b4ee02ca725
Michael Bond <mbond@the-forgotten.org>
2015-12-14 08:57:47 -0500

    Get json of all the projects in the system

---
+++ public_html/api/1.0/get/projects/index.php
@@ -0,0 +1,30 @@
+<?php
+include("../../../../header.php");
+
+
+try {
+
+	if (($projects = projects::get()) === FALSE) {
+		throw new Exception("error getting forms.");
+	}
+
+	$json = array();
+	foreach ($projects as $project) {
+		$json[] = array("ID" => $project['ID'], "title" => $project['projectName'], "shortTitle" => $project['projectID']);
+	}
+
+	print json_encode($json);
+
+	exit;
+}
+catch(Exception $e) {
+
+	print json_encode(array("error" => "true"));
+
+
+}
+
+exit;
+
+?>
+	
