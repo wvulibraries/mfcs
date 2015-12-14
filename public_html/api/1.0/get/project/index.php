8f41b93f9c02b193bdb1bdd4c4d0db9903f0dd48
Michael Bond <mbond@the-forgotten.org>
2015-12-14 10:32:55 -0500

    Objects in a project as json

---
+++ public_html/api/1.0/get/project/index.php
@@ -0,0 +1,39 @@
+<?php
+include("../../../../header.php");
+
+ini_set('memory_limit',-1);
+set_time_limit(0);
+
+// we don't need engine's display handling here. 
+$engine->obCallback = FALSE;
+
+try {
+
+	// ID is always passed into the API as "id" 
+	// set it as "formID" that the form class expects
+	http::setGet("formID",$engine->cleanGet['MYSQL']['id']);
+
+	if (!forms::validID()) {
+		throw new Exception("Invalid Form ID.");
+	}
+
+	if (($objects = objects::getAllObjectsForProject($engine->cleanGet['MYSQL']['id'])) === FALSE) {
+		throw new Exception("error getting objects.");
+	}
+
+	// we only return 1000? objects at a time. Calling application tells us where to start 
+	$objects = array_slice($objects, $engine->cleanGet['MYSQL']['start'], 1000);
+
+	print json_encode($objects);
+
+	exit;
+}
+catch(Exception $e) {
+
+	print json_encode(array("error" => "true", "message" => $e->getMessage()));
+
+}
+
+exit;
+
+?>
