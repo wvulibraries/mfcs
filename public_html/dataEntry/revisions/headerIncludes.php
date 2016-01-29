3cb2a4826be6ae6c4ee379deb10c34ea5dfd8e9e
ddavisgraphics <ddavisgraphics@gmail.com>
2016-01-29 11:23:33 -0500

    Style Mods for Revision

--- public_html/dataEntry/revisions/headerIncludes.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" type="text/css" href="{local var="siteRoot"}includes/css/mfcs.css">
-<link rel="stylesheet" type="text/css" href="{local var="siteRoot"}includes/css/revisions.css">
