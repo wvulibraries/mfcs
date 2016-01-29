0d6d4db8ba3b5eaeb6314072c68e9ceaa436210c
Michael Bond <mbond@the-forgotten.org>
2016-01-29 14:57:47 -0500

    pull specific actions from the logs

--- public_html/includes/classes/log.php
+++ public_html/includes/classes/log.php
@@ -24,6 +24,40 @@ class log {
 
 	}
 
+	// $actions = array of actions
+	public static function pull_actions($actions,$objectID) {
+
+		if (!is_array($actions)) {
+			return array();
+		}
+
+		$blame = array();
+
+		foreach ($actions as $action) {
+
+			$sql       = sprintf("SELECT `username`, `date` FROM `logs` WHERE `objectID`='%s' AND `action`='%s'",
+				mfcs::$engine->openDB->escape($objectID),
+				mfcs::$engine->openDB->escape($action)
+				);
+			$sqlResult = mfcs::$engine->openDB->query($sql);
+			
+			if (!$sqlResult['result']) {
+				errorHandle::newError(__METHOD__."() - : ".$sqlResult['error'], errorHandle::DEBUG);
+				return array();
+			}
+			
+			while($row = mysql_fetch_array($sqlResult['result'],  MYSQL_ASSOC)) {
+
+				$blame[] = array($row['username'],date('D, d M Y H:i',$row['date']));
+
+			}
+
+		}
+
+		return $blame;
+
+	}
+
 }
 
 ?>
