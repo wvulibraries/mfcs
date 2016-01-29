fb4d20f9327d043a5cde6eba6d1160a57e410a06
ddavisgraphics <ddavisgraphics@gmail.com>
2016-01-29 13:46:53 -0500

    Unhide menu

--- public_html/dataView/includes/listTemplates/initialSelectList.php
+++ public_html/dataView/includes/listTemplates/initialSelectList.php
@@ -1,5 +1,5 @@
 <div class="btn-group" role="group" aria-label="...">
-<!--     <a href="{phpself query="false"}?listType=all" class="btn btn btn-primary">All Objects</a>
- -->    <a href="{phpself query="false"}?listType=selectForm" class="btn btn btn-primary">Objects by Form</a>
+    <a href="{phpself query="false"}?listType=all" class="btn btn btn-primary">All Objects</a>
+    <a href="{phpself query="false"}?listType=selectForm" class="btn btn btn-primary">Objects by Form</a>
     <a href="{phpself query="false"}?listType=selectProject" class="btn btn btn-primary">Objects by Project</a>
 </div>
