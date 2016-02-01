7595d43ea595adb18ea8ad9f61fcda5d5d21dd9b
Michael Bond <mbond@the-forgotten.org>
2016-02-01 13:39:39 -0500

    cron task to scan files

---
+++ public_html/crons/virus_scan.php
@@ -0,0 +1,19 @@
+<?php
+
+session_save_path('/tmp');
+ini_set('memory_limit',-1);
+set_time_limit(0);
+
+require("../header.php");
+
+if (!isCLI()) {
+	print "Must be run from the command line.";
+	exit;
+}
+
+// Turn off EngineAPI template engine
+$engine->obCallback = FALSE;
+
+virus::process();
+
+?>
