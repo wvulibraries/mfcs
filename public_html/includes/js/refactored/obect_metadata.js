70709b732cb9604db6bc203d56e1faf5b961b070
ddavisgraphics <ddavisgraphics@gmail.com>
2016-02-03 13:59:36 -0500

    manual change for merge

--- public_html/includes/js/refactored/obect_metadata.js
+++ public_html/includes/js/refactored/obect_metadata.js
@@ -67,9 +67,9 @@ function submitMetadataModal() {
         if ($(this).attr("name") == "insertForm") {
             data = data + "&submitForm=Submit"
         }
-        else if ($(this).attr("name") == "updateForm") {
-            data = data + "&updateEdit=Update";
-        }
+        // else if ($(this).attr("name") == "updateForm") {
+        //     data = data + "&updateEdit=Update";
+        // }
 
         $.ajax({
             type: "POST",
