7f642f33026337e172b593f4d26ea5e006d23122
Michael Bond <mbond@the-forgotten.org>
2016-02-08 12:40:07 -0500

    updated navigation to use site root, broke out auditing.

--- public_html/includes/nav.php
+++ public_html/includes/nav.php
@@ -7,33 +7,41 @@
     <div class="close"> <a href="javascript:void(0);"> <i class="fa fa-times-circle"></i> </a> </div>
     <ul>
         <div>
-            <li><a href="/"> <i class="fa fa-home"></i> Home</a></li>
+            <li><a href="{local var="siteRoot"}"> <i class="fa fa-home"></i> Home</a></li>
         </div>
 
         <div>
             <li> <h3> Data Entry </h3> </li>
-            <li><a href="/dataEntry/selectForm.php"><i class="fa fa-angle-double-right"></i> Create</a></li>
-            <li><a href="/dataView/list.php?listType=selectForm"><i class="fa fa-angle-double-right"></i> List</a></li>
-            <li><a href="/dataView/search.php"><i class="fa fa-angle-double-right"></i> Search</a></li>
+            <li><a href="{local var="siteRoot"}dataEntry/selectForm.php"><i class="fa fa-angle-double-right"></i> Create</a></li>
+            <li><a href="{local var="siteRoot"}dataView/list.php?listType=selectForm"><i class="fa fa-angle-double-right"></i> List</a></li>
+            <li><a href="{local var="siteRoot"}dataView/search.php"><i class="fa fa-angle-double-right"></i> Search</a></li>
         </div>
 
         <div>
             <li> <h3> Form Management </h3> </li>
-            <li><a href="/formCreator/"> <i class="fa fa-angle-double-right"></i> New Form</a></li>
-            <li><a href="/formCreator/list.php"> <i class="fa fa-angle-double-right"></i> List Forms</a></li>
-            <li><a href="/formCreator/copy.php"> <i class="fa fa-angle-double-right"></i> Copy Form</a></li>
+            <li><a href="{local var="siteRoot"}formCreator/"> <i class="fa fa-angle-double-right"></i> New Form</a></li>
+            <li><a href="{local var="siteRoot"}formCreator/list.php"> <i class="fa fa-angle-double-right"></i> List Forms</a></li>
+            <li><a href="{local var="siteRoot"}formCreator/copy.php"> <i class="fa fa-angle-double-right"></i> Copy Form</a></li>
         </div>
 
         <div>
             <li> <h3> Administration </h3> </li>
             <!-- <li><a href="/admin/fileReProcessing/">File Re-Processing</a></li> -->
-            <li><a href="/admin/projects/"><i class="fa fa-angle-double-right"></i> Projects</a></li>
-            <li><a href="/admin/users/"><i class="fa fa-angle-double-right"></i> Users</a></li>
-            <li><a href="/admin/watermarks/"> <i class="fa fa-angle-double-right"></i> Watermarks</a></li>
-            <li><a href="/admin/metadataSchema/"> <i class="fa fa-angle-double-right"></i> Metadata Schemas </a></li>
-            <li><a href="/dashboard"> <i class="fa fa-angle-double-right"></i> Dashboard Statistics </a></li>
-            <li><a href="/stats"> <i class="fa fa-angle-double-right"></i> Statistics </a></li>
+            <li><a href="{local var="siteRoot"}admin/projects/"><i class="fa fa-angle-double-right"></i> Projects</a></li>
+            <li><a href="{local var="siteRoot"}admin/users/"><i class="fa fa-angle-double-right"></i> Users</a></li>
+            <li><a href="{local var="siteRoot"}admin/watermarks/"> <i class="fa fa-angle-double-right"></i> Watermarks</a></li>
+            <li><a href="{local var="siteRoot"}admin/metadataSchema/"> <i class="fa fa-angle-double-right"></i> Metadata Schemas </a></li>
         </div>
+
+        <div>
+            <li> <h3> System Information </h3> </li>
+            <li><a href="{local var="siteRoot"}dashboard/"><i class="fa fa-angle-double-right"></i> Dashboard</a></li>
+            <li><a href="{local var="siteRoot"}dashboard/permissions/"><i class="fa fa-angle-double-right"></i> Permissions Audit</a></li>
+            <li><a href="{local var="siteRoot"}dashboard/fixity/"> <i class="fa fa-angle-double-right"></i> Fixity History</a></li>
+            <li><a href="{local var="siteRoot"}dashboard/virus/"> <i class="fa fa-angle-double-right"></i> Virus History </a></li>
+            <li><a href="{local var="siteRoot"}stats/"> <i class="fa fa-angle-double-right"></i> Statistics </a></li>
+        </div>
+
         </div>
 
          <div>
