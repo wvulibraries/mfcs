62af69d784d971ad8c013fa88f3e6d34a533fb76
Michael Bond <mbond@the-forgotten.org>
2016-02-18 08:38:01 -0500

    text and breadcrumb changes

--- public_html/index.php
+++ public_html/index.php
@@ -21,6 +21,7 @@ $engine->eTemplate("include","header");
 	<ul class="breadcrumbs">
 			<li><a href="/">Home</a></li>
 			<li></li>
+			<li class="pull-right noDivider"><a href="https://github.com/wvulibraries/mfcs/wiki"> <i class="fa fa-book"></i> Documentation</a></li>
 	</ul>
 
 	<?php if($permission_denied) { ?>
@@ -36,7 +37,13 @@ $engine->eTemplate("include","header");
 			<img src="images/mfcsData.png" alt="mfcs holding a data icon" />
 		</div>
 		<div class="text">
-			<p> MFCS is a system built to store and archive digital projects, finding aids, and historical material entrusted to the library.  This system allows us to keep secure data, archive the original, and make modifications for other technologies to use.  The material ultimately ends up in a repository system such as Hydra, Islandora, or DLXS. </p>
+			<p>
+				MFCS is an 
+				<a href="https://en.wikipedia.org/wiki/Preservation_%28library_and_archival_science%29">archival and preservation</a> 
+				<a href="https://en.wikipedia.org/wiki/Digital_asset_management">Digital Asset Management System</a> 
+				developed by <a href="https://lib.wvu.edu/">WVU Libraries</a> since 2011. For more information
+				about MFCS visit the <a href="https://github.com/wvulibraries/mfcs">MFCS Github Project Page</a>.
+			</p>
 		</div>
 	</div>
 </section>
@@ -46,7 +53,12 @@ $engine->eTemplate("include","header");
 		<span class="span4">
 			<h2> <i class="fa fa-tasks"></i> Manage Inventory </h2>
 			<div class="text">
-				<p> MFCS allows you to manage forms, and forms that you have created.  Forms can be edited by using the list forms link in the Form Management section.  Editing objects works in a similar fashion using the Data Entry section.    </p> <br>
+				<p> 
+					MFCS allows you to manage forms, and forms that you have 
+					created.  Forms can be edited by using the list forms link in 
+					the Form Management section.  Editing objects works in a 
+					similar fashion using the Data Entry section.    
+				</p> <br>
 				<div class="actionButton">
 					<a href="/dataEntry/selectForm.php" class="btn btn-primary"> Create &amp; Edit Objects </a>
 					<a href="/dataView/search.php" class="btn btn-primary"> Search </a>
@@ -56,7 +68,12 @@ $engine->eTemplate("include","header");
 		<span class="span4">
 			<h2> <i class="fa fa-list-alt"></i> Manage Forms </h2>
 			<div class="text">
-				<p>Creating forms allow you to collect certain information along with your digital objects and is the basis for creating objects. Creating Objects in MFCS is the way your archive data for use in a digital repository later. </p><br>
+				<p>
+					Creating forms allow you to collect certain information along 
+					with your digital objects and is the basis for creating 
+					objects. Creating Objects in MFCS is the way your archive 
+					data for use in a digital repository later. 
+				</p><br>
 				<div class="actionButton">
 					<a href="/formCreator/" class="btn btn-primary"> Create A Form </a>
 					<a href="/formCreator/list.php" class="btn btn-primary"> List Forms </a>
@@ -66,7 +83,11 @@ $engine->eTemplate("include","header");
 		<span class="span4">
 			<h2> <i class="fa fa-book"></i> Documentation </h2>
 			<div class="text">
-				<p> Need help?  The documentation will help to provide some of the basic jargon and techniques to accomplishing tasks in MFCS.  </p><br>
+				<p> 
+					Need help?  The documentation will help to provide some of 
+					the basic jargon and techniques to accomplishing tasks in 
+					MFCS.
+				</p><br>
 				<div class="actionButton">
 					<a href="https://github.com/wvulibraries/mfcs/wiki" class="btn btn-primary"> MFCS Wiki </a>
 				</div>
