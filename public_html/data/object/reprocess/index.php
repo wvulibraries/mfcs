4ae8e82cabd16fb5c10de9692059b966eb2af6c0
Michael Bond <mbond@the-forgotten.org>
2016-02-24 07:00:11 -0500

    reprocess a single object

--- public_html/data/object/reprocess/index.php
+++ public_html/data/object/reprocess/index.php
@@ -14,20 +14,28 @@ try {
 	// handle submission
 	if (isset($engine->cleanGet['MYSQL']['confirm']) &&
 		$engine->cleanGet['MYSQL']['confirm'] == $engine->cleanGet['MYSQL']['objectID']) {
+		$object = objects::get($engine->cleanGet['MYSQL']['objectID']);
+		$fields = forms::get_file_fields($object['formID']);
 
+		// start transactions
+		if (mfcs::$engine->openDB->transBegin("objects") !== TRUE) {
+			throw new Exception("unable to start database transactions");
+		}
+
+		foreach ($fields as $field) {
+			if (files::insert_into_processing_table($engine->cleanGet['MYSQL']['objectID'],$field['name']) === FALSE) {
+				mfcs::$engine->openDB->transRollback();
+				mfcs::$engine->openDB->transEnd();
+				throw new Exception("Error inserting for reprocessing.");
+			}
+		}
+
+		mfcs::$engine->openDB->transCommit();
+		mfcs::$engine->openDB->transEnd();
+
+		$confirmed = TRUE;
 	}
-	else {
-		// @todo: This should really be in the object class, but since i'm back porting to production
-		// i'm trying to avoid modifying the classes as much as possible.
-		$object_title_field = forms::getObjectTitleField($engine->cleanGet['MYSQL']['formID']);
-		$object             = objects::get($engine->cleanGet['MYSQL']['objectID']);
 
-		localvars::add("metadata_title",$object['data'][$object_title_field]);
-
-	}
-
-	localvars::add("form_title", forms::title($engine->cleanGet['MYSQL']['formID']));
-	localvars::add("formID",$engine->cleanGet['MYSQL']['formID']);
 	localvars::add("objectID",$engine->cleanGet['MYSQL']['objectID']);
 	localvars::add("php_self",$_SERVER['PHP_SELF']); // i should back-port the php_Self module to engine 3 (or upgrade MFCS to engine 4)
 
@@ -51,6 +59,9 @@ $engine->eTemplate("include","header");
 
 	<ul class="breadcrumbs">
 		<li><a href="{local var="siteRoot"}">Home</a></li>
+		<li><a href="{local var="siteRoot"}dataEntry/selectForm.php">Select a Form</a></li>
+		<li><a href="{local var="siteRoot"}dataEntry/object.php?objectID={local var="objectID"}">Object</a></li>
+		<li class="pull-right noDivider"><a href="https://github.com/wvulibraries/mfcs/wiki/Reprocessing" target="_blank"> <i class="fa fa-book"></i> Documentation</a></li>
 	</ul>
 
 
@@ -75,12 +86,13 @@ $engine->eTemplate("include","header");
 			</p>
 		</span>
 
-		<a class="delete_metadata_confirm" href="{local var="php_self"}?objectID={local var="objectID"}&confirm={local var="objectID"}"><i class="fa fa-trash"></i>Confirm Delete</a> &nbsp;
-		<a class="delete_metadata_cancel" href="{local var="siteRoot"}/data/metadata/edit/?objectID={local var="objectID"}"><i class="fa fa-times"></i>Cancel</a>
+		<a class="delete_metadata_confirm" href="{local var="php_self"}?objectID={local var="objectID"}&confirm={local var="objectID"}"><i class="fa fa-arrow-circle-o-right"></i>Confirm Reprocessing</a> &nbsp;
 
 		<?php } else { ?>
 
-		<a href="{local var="siteRoot"}dataEntry/metadata.php?formID={local var="formID"}">Return to {local var="form_title"} Form page</a>
+		<p>Object inserted for Reprocessing.</p>
+
+		<a href="{local var="siteRoot"}dataEntry/object.php?objectID={local var="objectID"}">Return to Object Editing page</a>
 
 		<?php } ?>
 	</div>
