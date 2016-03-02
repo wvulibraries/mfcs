0e86d8493266c7e12343eda2cf382d1c714d66da
Michael Bond <mbond@the-forgotten.org>
2016-03-02 12:45:18 -0500

    Convert from list to a table

--- public_html/exports/Dublin_Core/index.php
+++ public_html/exports/Dublin_Core/index.php
@@ -4,22 +4,38 @@ include "../../header.php";
 
 $forms = forms::getForms(TRUE);
 
-$form_list = "<ul>";
+$form_list = array();
 foreach ($forms as $form) {
-  $form_list .= sprintf('<li><a href="%sexports/Dublin_Core/standard/?formID=%s">%s</a></li>',
-                        localvars::get("siteRoot"),
-                        $form['ID'],
-                        forms::title($form['ID'])
-                      );
-}
-$form_list .= "</ul>";
 
-localvars::add("form_list",$form_list);
+  $form_list[] = array(
+    "formName" => forms::title($form['ID']),
+    "normal"   => sprintf('<li><a href="%sexports/Dublin_Core/standard/?formID=%s">Normal</a></li>',
+                        localvars::get("siteRoot"),
+                        $form['ID']
+                      ),
+    "nocombine" => sprintf('<li><a href="%sexports/Dublin_Core/standard/?formID=%s&nocombine=true">Ignore Combines</a></li>',
+                      localvars::get("siteRoot"),
+                      $form['ID']
+                    )
+                  );
+}
+
+$table           = new tableObject("array");
+$table->sortable = TRUE;
+$table->summary  = "Dublin Core export Listing";
+$table->class    = "styledTable";
+$table->headers(array("Form Name","Normal DC Export","DC Export Ignoring Combines"));
+
+localvars::add("form_list",$table->display($form_list));
 
 $engine->eTemplate("include","header");
 ?>
 
 <section>
+  <header class="page-header">
+    <h1 class="page-title">Dublin Core Exports</h1>
+  </header>
+
 <p>Select a form to export as dublin core.</p>
 <p>Please note that only forms with Dublin Core defined will work with this export.</p>
 
