553165a580ba9c09179287113c29bd887139eb83
ddavisgraphics <ddavisgraphics@gmail.com>
2016-04-18 21:07:00 -0400

    setting up logic.

--- public_html/data/object/batchUpload/process/index.php
+++ public_html/data/object/batchUpload/process/index.php
@@ -1,7 +1,7 @@
 <?php
 // header
 include("../../../../header.php");
-$engine->eTemplate("include","header");
+//$engine->eTemplate("include","header");
 
 //Permissions Access
 if(!mfcsPerms::evaluatePageAccess(1)){
@@ -11,13 +11,45 @@ if(!mfcsPerms::evaluatePageAccess(1)){
 // Batch Uploading Logs
 log::insert("BatchUpload",0,0, "Processing upload");
 
+
 print "<pre>";
-var_dump($engine->cleanPost);
+var_dump($engine->cleanPost['MYSQL']);
 print "</pre>";
+
+
+// get file ID and Path
+if(isset($engine->cleanPost['MYSQL']['FileUploadBox'])){
+
+    $fileUploadID    = $engine->cleanPost['MYSQL']['FileUploadBox'];
+    $uploadDirectory = "/home/mfcs.lib.wvu.edu/data/working/uploads/$fileUploadID/";
+
+    // loop through directory to get file information
+    $directory       = new DirectoryIterator($uploadDirectory);
+
+    foreach ($directory as $file) {
+        if($file->isFile() && !$file->isDot()){
+            print "<pre>";
+            var_dump($file->getFilename() . "\n");
+            print "</pre>";
+
+            print "<pre>";
+            var_dump($file->getSize() . "\n");
+            print "</pre>";
+        }
+    }
+}
+
+// insert the file into a new object
+
+// make sure that the object exsists and is searchable
+
+// should there be a check to make sure that fixity is being run
+
+// should there be something that puts the file into the file processing (is done on insert?)
 
 ?>
 
 
 <?php
-$engine->eTemplate("include","footer");
+//$engine->eTemplate("include","footer");
 ?>
