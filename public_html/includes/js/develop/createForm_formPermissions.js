844b6104ac564eea42b11a8389418a1dc1906d9d
Michael Bond <mbond@the-forgotten.org>
2016-05-23 07:41:22 -0400

    fix permissions bug .. ?

--- public_html/includes/js/develop/createForm_formPermissions.js
+++ public_html/includes/js/develop/createForm_formPermissions.js
@@ -10,6 +10,22 @@ $(function() {
 // Helper Functions
 // =================================================================
 
+function addItemToID(id, item) {
+    var theSelect = document.getElementById(id);
+
+    if (item.value == "null") {
+        return;
+    }
+
+    for (i = theSelect.length - 1; i >= 0; i--) {
+        if (theSelect.options[i].value == item.value) {
+            return;
+        }
+    }
+
+    theSelect.options[theSelect.length] = new Option(item.text, item.value);
+}
+
 function removeItemFromID(id, item) {
 	var selIndex = item.selectedIndex;
 	if (selIndex != -1) {
