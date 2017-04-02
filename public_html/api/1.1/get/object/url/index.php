6f427a726bb04a4a8f50565210c8e83de66230c8
Michael Bond <mbond@the-forgotten.org>
2017-04-02 07:08:35 -0400

    Add API Endpoint for getting a URL for an oobject

---
+++ public_html/api/1.1/get/object/url/index.php
@@ -0,0 +1,22 @@
+<?php
+include("../../../.././header.php");
+// TODO this should be included in general object retrievial ... but I don't want
+// to change up how that works, without better unit testing in place.
+// Having this included in general object retrievial doesn't remove the need
+// for this endpoint.
+
+// we don't need engine's display handling here.
+$engine->obCallback = FALSE;
+
+try {
+	if (!objects::validID(TRUE,$engine->cleanGet['MYSQL']['id'])) throw new Exception("Invalid Object ID.");
+	if (($object_url = objects::getUrl($engine->cleanGet['MYSQL']['id'])) === FALSE) throw new Exception("error getting object.");
+	$json = sprintf('{"url": "%s"}', $object_url);
+	print (isset($engine->cleanGet['HTML']['prettyPrint']))?json_format($json):$json;
+	exit;
+}
+catch(Exception $e) {
+	print json_encode(array("error" => "true", "message" => $e->getMessage()));
+}
+exit;
+?>
