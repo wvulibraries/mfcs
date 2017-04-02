16a7c55fea5db2e4984ee70357c943dacfcc0e21
Michael Bond <mbond@the-forgotten.org>
2017-04-02 12:28:23 -0400

    Max length of varchar in version of mysql on servers is 767
    MySQL needs updated on the servers

--- SQLFiles/migrations/2017.02.25.0808.sql
+++ SQLFiles/migrations/2017.02.25.0808.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `objectUrls`;
 CREATE TABLE `objectUrls` (
   `objectID` int(10) unsigned NOT NULL,
-  `url` varchar(1000) NOT NULL,
+  `url` varchar(767) NOT NULL,
   PRIMARY KEY (`objectID`),
   UNIQUE KEY `url` (`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
