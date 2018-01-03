e6f3681b4bde7e34d74874119f17e24764c5ed55
ddavisgraphics <dave@ddavisgraphics.com>
2018-01-03 10:24:14 -0500

    add max allowed packet for vagrant ups.

--- SQLFiles/setup.sql
+++ SQLFiles/setup.sql
@@ -1,4 +1,5 @@
 DROP DATABASE IF EXISTS `mfcs`;
 CREATE DATABASE IF NOT EXISTS `mfcs`;
 GRANT ALL PRIVILEGES ON `mfcs`.* TO 'username'@'localhost';
+SET GLOBAL max_allowed_packet=1073741824;
 USE `mfcs`;
