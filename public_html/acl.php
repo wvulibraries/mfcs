9cd73577460e20671417e4cfe16a857df8efc26e
ddavisgraphics <dave@ddavisgraphics.com>
2018-09-04 08:55:45 -0400

    New AD group.

--- public_html/acl.php
+++ public_html/acl.php
@@ -4,6 +4,7 @@
 
 	$engine->accessControl("ADgroup","libraryGroup_staff",TRUE,FALSE);
 	$engine->accessControl("ADgroup","libraryGroup_students",TRUE,FALSE);
+	$engine->accessControl("ADgroup","ibraryGroup_AppV_WVCStudents",TRUE,FALSE);
 	$engine->accessControl("denyAll");
 	$engine->accessControl("build");
 
