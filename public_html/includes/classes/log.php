947b498e4ab0f810f00797ca9f15267e461f8ca8
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2024-04-23 17:29:13 -0400

    revert log.php

--- public_html/includes/classes/log.php
+++ public_html/includes/classes/log.php
@@ -1,63 +1,61 @@
 <?php
 
 class log {
-	public static function insert($action, $objectID, $formID, $info = NULL) {
-		$db = mfcs::$engine->openDB;
+
+	public static function insert($action,$objectID=0,$formID=0,$info=NULL) {
+
+		$sql       = sprintf("INSERT INTO `logs` (`username`,`IP`,`action`,`objectID`,`formID`,`info`,`date`) VALUES('%s','%s','%s','%s','%s','%s','%s')",
+			mfcs::$engine->openDB->escape(users::user('username')),
+			mfcs::$engine->openDB->escape($_SERVER['REMOTE_ADDR']),
+			mfcs::$engine->openDB->escape($action),
+			mfcs::$engine->openDB->escape($objectID),
+			mfcs::$engine->openDB->escape($formID),
+			mfcs::$engine->openDB->escape($info),
+			time()
+			);
+		$sqlResult = mfcs::$engine->openDB->query($sql);
 		
-		// Validate parameters
-		if (empty($action)) {
-			errorHandle::newError(__METHOD__."() - Action cannot be empty", errorHandle::DEBUG);
+		if (!$sqlResult['result']) {
+			errorHandle::newError(__METHOD__."() - : ".$sqlResult['error'], errorHandle::DEBUG);
 			return FALSE;
 		}
-	
-		// Escape values
-		$username = $db->escape(users::user('username'));
-		$ip = $db->escape($_SERVER['REMOTE_ADDR']);
-		$action = $db->escape($action);
-		$objectID = $db->escape($objectID);
-		$formID = $db->escape($formID);
-		$info = $db->escape($info);
-		$date = time();
-	
-		// Build the SQL query
-		$sql = "INSERT INTO `logs` (`username`, `IP`, `action`, `objectID`, `formID`, `info`, `date`) 
-				VALUES ('$username', '$ip', '$action', '$objectID', '$formID', '$info', '$date')";
 		
-		// Execute the query
-		$sqlResult = $db->query($sql);
-	
-		// Check for insertion success
-		if ($sqlResult['result']) {
-			return TRUE;
-		} else {
-			errorHandle::newError(__METHOD__."() - Insertion failed: ".$sqlResult['error'], errorHandle::DEBUG);
-			return FALSE;
-		}
+		return TRUE;
+
 	}
 
 	// $actions = array of actions
-	public static function pull_actions($actions, $objectID) {
-		if (!is_array($actions) || empty($actions)) {
-			return [];
+	public static function pull_actions($actions,$objectID) {
+
+		if (!is_array($actions)) {
+			return array();
 		}
-	
-		$blame = [];
-		$db = mfcs::$engine->openDB; // Assuming $db is your database connection object
-	
-		$actionList = "'" . implode("','", array_map([$db, 'escape'], $actions)) . "'";
-		$sql = "SELECT `username`, `date` FROM `logs` WHERE `objectID`='{$db->escape($objectID)}' AND `action` IN ({$actionList})";
-		$sqlResult = $db->query($sql);
-	
-		if (!$sqlResult['result']) {
-			errorHandle::newError(__METHOD__ . "() - " . $sqlResult['error'], errorHandle::DEBUG);
-			return [];
+
+		$blame = array();
+
+		foreach ($actions as $action) {
+
+			$sql       = sprintf("SELECT `username`, `date` FROM `logs` WHERE `objectID`='%s' AND `action`='%s'",
+				mfcs::$engine->openDB->escape($objectID),
+				mfcs::$engine->openDB->escape($action)
+				);
+			$sqlResult = mfcs::$engine->openDB->query($sql);
+			
+			if (!$sqlResult['result']) {
+				errorHandle::newError(__METHOD__."() - : ".$sqlResult['error'], errorHandle::DEBUG);
+				return array();
+			}
+			
+			while($row = mysql_fetch_array($sqlResult['result'],  MYSQL_ASSOC)) {
+
+				$blame[] = array($row['username'],date('D, d M Y H:i',$row['date']));
+
+			}
+
 		}
-	
-		while ($row = mysql_fetch_array($sqlResult['result'], MYSQL_ASSOC)) {
-			$blame[] = [$row['username'], date('D, d M Y H:i', $row['date'])];
-		}
-	
+
 		return $blame;
+
 	}
 
 }
