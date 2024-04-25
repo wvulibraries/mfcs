7a56fb10e6b85c59ed04031f2884db58407578fe
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2024-04-25 09:43:40 -0400

    updated file viewer - refactored

--- public_html/includes/fileViewer.php
+++ public_html/includes/fileViewer.php
@@ -5,7 +5,7 @@ include "../header.php";
 $engine->obCallback = false;
 
 // List of Mime types
-$videoMimeTypes = array( 'application/mp4', 'application/ogg', 'video/3gpp', 'video/3gpp2', 'video/flv', 'video/h264', 'video/mp4', 'video/mpeg', 'video/mpeg-2', 'video/mpeg4', 'video/ogg', 'video/ogm', 'video/quicktime', 'video/avi');
+$videoMimeTypes = array('application/mp4', 'application/ogg', 'video/3gpp', 'video/3gpp2', 'video/flv', 'video/h264', 'video/mp4', 'video/mpeg', 'video/mpeg-2', 'video/mpeg4', 'video/ogg', 'video/ogm', 'video/quicktime', 'video/avi');
 $audioMimeTypes = array('audio/acc', 'audio/mp4', 'audio/mp3', 'audio/mp2', 'audio/mpeg', 'audio/oog', 'audio/midi', 'audio/wav', 'audio/x-ms-wma','audio/webm');
 $imageMimeTypes = array('image/jpeg', 'image/gif', 'image/png', 'image/bmp', 'image/tiff', 'image/x-icon');
 $pdfMimeTypes   = array('application/pdf');
@@ -34,19 +34,9 @@ try {
     if (strpos($fileType, 'combined') !== false) {
         $filepath = '';
         if ($fileType == 'combinedPDF') {
-            foreach ($fileArray['files']['combine'] as $file) {
-                if (strpos($file['type'], 'application/') !== false) {
-                    $filepath = files::getSaveDir($fileUUID, 'combine') . $file['name'];
-                    break;
-                }
-            }
+            $filepath = getCombinedFilePath($fileArray['files']['combine'], 'application/');
         } else {
-            foreach ($fileArray['files']['combine'] as $file) {
-                if (strpos($file['type'], 'image/') !== false) {
-                    $filepath = files::getSaveDir($fileUUID, 'combine') . $file['name'];
-                    break;
-                }
-            }
+            $filepath = getCombinedFilePath($fileArray['files']['combine'], 'image/');
         }
     } else {
         $fileID = $engine->cleanGet['MYSQL']['fileID'];
@@ -60,41 +50,29 @@ try {
     }
 
     // Check if file is OCR type and handle it
-	if ($fileType == 'ocr') {
-		// get position of file in array
-		$fileID = $engine->cleanGet['MYSQL']['fileID'];
+    if ($fileType == 'ocr') {
+        $fileID = $engine->cleanGet['MYSQL']['fileID'];
+        $ocrFile = $fileArray['files']['ocr'][$fileID]["ocr"][0]['name'];
 
-		// get OCR file name
-		$ocrFile = $fileArray['files']['ocr'][$fileID]["ocr"][0]['name'];
+        if (!empty($ocrFile)) {
+            $filepath = ($fileType == 'archive') ? files::getSaveDir($fileUUID, $fileType) . DIRECTORY_SEPARATOR . $file['name'] : files::getSaveDir($fileUUID, $fileType) . $ocrFile;;
 
-		if (!empty($ocrFile)) {
-			// create full path to file
-			$filepath = ($fileType == 'archive') ? files::getSaveDir($fileUUID, $fileType) . DIRECTORY_SEPARATOR . $file['name'] : files::getSaveDir($fileUUID, $fileType) . $ocrFile;;
-
-			// Check if the OCR file path exists
-			if (file_exists($filepath)) {
-				// Set headers and output OCR file content
-				header(sprintf("Content-Disposition: attachment; filename=%s", basename($filepath)));
-				header("Content-Type: text/plain");
-				ini_set('memory_limit', '-1');
-				die(file_get_contents($filepath));
-			} else {
-				throw new Exception('OCR File not found: ' . $filepath);
-			}
-		} else {
-			throw new Exception('No OCR files found for this object.');
-		}
-	}
+            if (file_exists($filepath)) {
+                downloadFile($filepath);
+            } else {
+                throw new Exception('OCR File not found: ' . $filepath);
+            }
+        } else {
+            throw new Exception('No OCR files found for this object.');
+        }
+    }
 
     // Get MIME Type
     $mimeType = mime_content_type($filepath);
 
     // Set headers and output file content
     if (isset($engine->cleanGet['MYSQL']['download']) && str2bool($engine->cleanGet['MYSQL']['download'])) {
-        header(sprintf("Content-Disposition: attachment; filename=%s", basename($filepath)));
-        header("Content-Type: application/octet-stream");
-        ini_set('memory_limit', '-1');
-        die(file_get_contents($filepath));
+        downloadFile($filepath);
     } else {
         if ($mimeType == 'application/x-empty') {
             throw new Exception('Failed to locate file to display: ' . $filepath);
@@ -111,5 +89,21 @@ try {
 } catch (Exception $e) {
     errorHandle::newError($e->getMessage(), errorHandle::DEBUG);
     die($e->getMessage());
+}
+
+function getCombinedFilePath($files, $type) {
+    foreach ($files as $file) {
+        if (strpos($file['type'], $type) !== false) {
+            return files::getSaveDir($fileUUID, 'combine') . $file['name'];
+        }
+    }
+    return '';
+}
+
+function downloadFile($filepath) {
+    header(sprintf("Content-Disposition: attachment; filename=%s", basename($filepath)));
+    header("Content-Type: application/octet-stream");
+    ini_set('memory_limit', '-1');
+    die(file_get_contents($filepath));
 }
 ?>
