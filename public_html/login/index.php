70c123326b49cf7019c1d750f53ec22b4efe3d5f
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2024-06-13 15:28:20 -0400

    updated index.php login page

--- public_html/login/index.php
+++ public_html/login/index.php
@@ -4,110 +4,110 @@ $engine = EngineAPI::singleton();
 
 errorHandle::errorReporting(errorHandle::E_ALL);
 
-$engine->dbConnect("database","mfcs",TRUE);
+$engine->dbConnect("database", "mfcs", TRUE);
 
 require_once "../includes/index.php";
 mfcs::singleton();
 
+// Start session
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 // Login Type
 $loginType = "ldap";
 // Domain for ldap login
-$engine->localVars("domain","wvu-ad");
+$engine->localVars("domain", "wvu-ad");
 
-$authFail  = FALSE; // Authorization to the current resource .. we may end up not using this
+$authFail = FALSE; // Authorization to the current resource .. we may end up not using this
 $loginFail = FALSE; // Login Success/Failure
 
 if (isset($engine->cleanGet['HTML']['page'])) {
-	$page = $engine->cleanGet['HTML']['page'];
-	if (isset($engine->cleanGet['HTML']['qs'])) {
-		$qs = urldecode($engine->cleanGet['HTML']['qs']);
-		$qs = preg_replace('/&amp;amp;/','&',$qs);
-		$qs = preg_replace('/&amp;/','&',$qs);
-	}
-	else {
-		$qs = "";
-	}
+    $page = $engine->cleanGet['HTML']['page'];
+    if (isset($engine->cleanGet['HTML']['qs'])) {
+        $qs = urldecode($engine->cleanGet['HTML']['qs']);
+        $qs = preg_replace('/&amp;amp;/', '&', $qs);
+        $qs = preg_replace('/&amp;/', '&', $qs);
+    } else {
+        $qs = "";
+    }
 }
 
-//Login processing:
+// Login processing:
 if (isset($engine->cleanPost['HTML']['loginSubmit'])) {
-	if (!isset($engine->cleanPost['HTML']['username']) || !isset($engine->cleanPost['HTML']['password'])) {
-		$authFail  = TRUE;
-		$loginFail = TRUE;
-	}
-	else {
+    if (!isset($engine->cleanPost['HTML']['username']) || !isset($engine->cleanPost['HTML']['password'])) {
+        $authFail = TRUE;
+        $loginFail = TRUE;
+    } else {
+        global $engineVars, $loginFunctions;
 
-		global $engineVars;
-		if ($engine->login($loginType)) {
-			log::insert("Login");
-            if(isset($engine->cleanGet['HTML']['url'])) {
-				header("Location: ".$engine->cleanGet['HTML']['URL'] ) ;
-			}
-			else {
-				if (isset($page)) {
-					header("Location: ".$page."?".$qs );
-				}
-				else {
-					header("Location: ".$engineVars['WEBROOT'] );
-				}
+        // Validate login function exists
+        if (isset($loginFunctions[$loginType])) {
+            $loginFunction = $loginFunctions[$loginType];
+            $username = trim($engine->cleanPost['HTML']['username']);
+            $password = $engine->cleanPost['HTML']['password'];
 
-			}
-		}
-		else {
-			log::insert("Login Failure");
-			$loginFail = TRUE;
-		}
-
-	}
-
+            // Call the appropriate login function
+            if ($loginFunction($username, $password)) {
+                log::insert("Login");
+                if (isset($engine->cleanGet['HTML']['url'])) {
+                    header("Location: " . $engine->cleanGet['HTML']['url']);
+                    exit; // Ensure no further code is executed
+                } else {
+                    if (isset($page)) {
+                        header("Location: " . $page . "?" . $qs);
+                        exit; // Ensure no further code is executed
+                    } else {
+                        header("Location: " . $engineVars['WEBROOT']);
+                        exit; // Ensure no further code is executed
+                    }
+                }
+            } else {
+                log::insert("Login Failure");
+                $loginFail = TRUE;
+            }
+        } else {
+            $loginFail = TRUE; // Login function not found
+        }
+    }
 }
-
 ?>
 <!-- nagios check -->
 <html>
 <head>
-	<title>MFCS Login</title>
-	<link rel="stylesheet" type="text/css" href="/css/login.css"/>
+    <title>MFCS Login</title>
+    <link rel="stylesheet" type="text/css" href="/css/login.css"/>
 </head>
-
 <body>
-
-<!-- <header>
-	<h1>Metadata Form Creation System</h1>
-</header> -->
-
-
 <div id="loginBox">
-
-	<form name="loginForm" action="{phpself query="false"}<?php if(isset($page)){ echo "?page=".$page; if(isset($qs)) { echo "&qs=".(urlencode($qs)); } } ?>" method="post">
-		<div class="formHeader">
-			<h2> MFCS Login</h2>
-		</div>
-		{engine name="insertCSRF"}
-		<?php
-	if($loginFail) {
-		print "<div class='error'><p>Login Failed. User name or Password is incorrect.</p></div>";
-	}
-	if(isset($page)) {
-		print "<div class='error'><p>You are either not logged in or do not have access to the requested page.</p></div>";
-	}
-	?>
-
-
-		<label for="username" class="hidden">Username:</label>
-		<input type="text" name="username" id="username" class="styledInput" value="" autofocus="autofocus" placeholder="username" /> <br /><br />
-		<label for="password" class="hidden">Password:</label>
-		<input type="password" name="password" id="password" placeholder="password" class="styledInput"  value="" onkeypress="capsLockCheck(event);"/> <span id="capsLock" style="display:none;">Caps Lock is On</span>
-		<br/>
-		<div class="alignSubmit">
-			<input type="submit" class="styledInput" name="loginSubmit" id="submitButton" value="Login" />
-		</div>
-	</form>
+    <form name="loginForm" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); if (isset($page)) { echo "?page=" . $page; if (isset($qs)) { echo "&qs=" . urlencode($qs); } } ?>" method="post">
+        <div class="formHeader">
+            <h2> MFCS Login</h2>
+        </div>
+        {engine name="insertCSRF"}
+        <?php
+        if ($loginFail) {
+            echo "<div class='error'><p>Login Failed. Username or Password is incorrect.</p></div>";
+        }
+        if (isset($page)) {
+            echo "<div class='error'><p>You are either not logged in or do not have access to the requested page.</p></div>";
+        }
+        ?>
+        <label for="username" class="hidden">Username:</label>
+        <input type="text" name="username" id="username" class="styledInput" value="" autofocus="autofocus" placeholder="username" /> <br /><br />
+        <label for="password" class="hidden">Password:</label>
+        <input type="password" name="password" id="password" placeholder="password" class="styledInput" value="" onkeypress="capsLockCheck(event);" /> <span id="capsLock" style="display:none;">Caps Lock is On</span>
+        <br/>
+        <div class="alignSubmit">
+            <input type="submit" class="styledInput" name="loginSubmit" id="submitButton" value="Login" />
+        </div>
+    </form>
 </div>
-
 <div class="mfcs-logo">
-	<img src="/images/mfcs.png" alt="Metadata Form Creation System" />
+    <img src="/images/mfcs.png" alt="Metadata Form Creation System" />
 </div>
-
 </body>
 </html>
+
+
+
