cc98b5225ff58f35031ccab6cb4c9168ccade59c
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2024-08-23 15:28:40 -0400

    added example usage to api_acl.php

--- config/api_acl.php
+++ config/api_acl.php
@@ -6,4 +6,9 @@
 // this file should be sylinked to your mfcs base directory, example:
 // ln -s /home/mfcs.lib.wvu.edu/git_pull/mfcs/api_acl.php /home/mfcs.lib.wvu.edu/api_acl.php
 
+// example usage:
+// $engine->accessControl("accessControl_ip_checkIPAddr","xxx.xxx.xxx.*",TRUE,FALSE);
+// $engine->accessControl("denyAll");
+// $engine->accessControl("build");
+
 ?>
